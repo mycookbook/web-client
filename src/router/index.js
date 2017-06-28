05d991b4a7ba6e7fa6e005010e94cbe82fef9869
andela-fokosun <okosunuzflorence@gmail.com>
2017-06-28 22:10:53 +0100

    WIP

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueResource from 'vue-resource'
 import Hello from '@/components/Hello'
+import SignIn from '../components/SignIn.vue'
+import SignOut from '../components/SignOut.vue'
 
+Vue.use(VueResource)
 Vue.use(Router)
 
 export default new Router({
@@ -9,5 +13,16 @@ export default new Router({
     path: '/',
     name: 'Hello',
     component: Hello
+  }, {
+    path: '/signin',
+    name: 'SignIn',
+    component: SignIn
+  }, {
+    path: '/signout',
+    name: 'Signout',
+    component: SignOut
+  }, {
+    path: '/user/create',
+    name: 'user.create'
   }]
 })
