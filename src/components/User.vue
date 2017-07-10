ce2ccc9f849229ae21e5a56a82fbd87568ea1d80
andela-fokosun <okosunuzflorence@gmail.com>
2017-07-10 19:03:32 +0100

    :construction: implement user component

---
+++ src/components/User.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ $route.params.username }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        // We need to initialize the component with any
+        // properties that will be used in it
+        username: ''
+      }
+    }
+  }
+  </script>
