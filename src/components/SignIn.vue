39424225a2b65225eceef664fd54ce76f0273545
andela-fokosun <okosunuzflorence@gmail.com>
2017-09-24 23:56:35 +0300

    wip: TODO: implement state management

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -2,71 +2,84 @@
     <div class="ui grid" style="margin:auto">
       <Navigation></Navigation>
       <div class="four wide column centered grid">
-          <h1>Log in to your account</h1>
+          <h1>Log in</h1>
           <p>Don't have an account?
             <router-link to="/">Sign Up</router-link>
         </p>
-        <form class="ui form">
-          <div class="field">
-            <label>Email</label>
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Enter your username"
-              v-model="credentials.username"
-            >
-          </div>
-          <div class="field">
-            <label>Password</label>
-            <input
-              type="password"
-              class="form-control"
-              placeholder="Enter your password"
-              v-model="credentials.password"
-            >
-          </div>
-          <!-- <button class="ui button" type="submit">Submit</button> -->
-          <div class="item">
-            <button class="ui primary button" @click="logIn">Log In</button>
-          </div>
-        </form>
+        <template>
+          <form class="ui form" v-on:submit.prevent="logIn">
+            <div class="ui negative message" v-if="info">
+              <div class="header">
+                There were some errors with your submission
+              </div>
+              <p>
+                <ul class="list">
+                  <li v-for="error in errors">
+                    {{error}}
+                  </li>
+                </ul>
+              </p>
+            </div>
+            <div class="field">
+              <label>Email</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Enter your email"
+                v-model="credentials.email"
+              >
+            </div>
+            <div class="field">
+              <label>Password</label>
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Enter your password"
+                v-model="credentials.password"
+              >
+            </div>
+            <!-- <button class="ui button" type="submit">Submit</button> -->
+            <div class="item">
+              <button class="ui primary button" @click="logIn">Log In</button>
+            </div>
+          </form>
+        </template>
       </div>
     </div>
   </template>
 
   <script>
     import Navigation from './Navigation.vue'
-    // import auth from '../auth'
-    const API_URL = 'http://localhost:3031/'
-    const LOGIN_URL = API_URL + 'user/create/'
+    import router from '@/router'
+    // import store from 'store'
     export default {
       data () {
         return {
           // We need to initialize the component with any
           // properties that will be used in it
           credentials: {
-            username: '',
+            email: '',
             password: ''
           },
-          error: ''
+          info: false,
+          error: '',
+          dev: 'http://api.dev/api/v1/signin',
+          prod: 'https://lit-eyrie-53695.herokuapp.com/api/v1/signin',
+          errors: ['one', 'two']
         }
       },
       methods: {
         logIn: function (e) {
-          e.preventDefault()
-          var credentials = {
-            username: this.credentials.username,
+          this.$http.post(this.dev, {
+            email: this.credentials.email,
             password: this.credentials.password
-          }
-
-          // We need to pass the component's this context
-          // to properly make use of http in the auth service
-          // auth.login(this, credentials, 'secretquote')
-
-          this.$http.post(LOGIN_URL, {credentials}).then((response) => {
-            console.log(response, 'success')
+          }).then((response) => {
+            localStorage.setItem('token', response.body.token)
+            // store.commit('LOGIN_USER')
+            router.push('/')
           }, (response) => {
-            console.log(response, 'error')
+            this.info = true
+            this.errors = JSON.parse(response.bodyText)
           })
         }
       },
