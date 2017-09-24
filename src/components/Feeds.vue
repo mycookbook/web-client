e9d275ec8b8e65fbc2ca7bbadda5eadee7652fb0
andela-fokosun <okosunuzflorence@gmail.com>
2017-09-25 00:40:08 +0300

    wip: build user feed component

---
+++ src/components/Feeds.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="ui grid" style="margin:auto">
+      <Navigation></Navigation>
+      I am feeds
+    </div>
+  </template>
+
+  <script>
+    import Navigation from './Navigation.vue'
+    export default {
+      data () {
+        return {
+
+        }
+      },
+      methods: {
+      },
+      components: {
+        'Navigation': Navigation
+      }
+    }
+    </script>
