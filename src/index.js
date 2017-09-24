9c4e7af9859014ac730d49c21122c52ac9064982
andela-fokosun <okosunuzflorence@gmail.com>
2017-09-24 23:55:46 +0300

    removed unused code

--- src/index.js
+++ src/index.js
@@ -1,31 +1,14 @@
 import Vue from 'vue'
 import App from './components/App.vue'
-import Home from './components/Home.vue'
-import SecretQuote from './components/SecretQuote.vue'
-import Signup from './components/Signup.vue'
-import Login from './components/Login.vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+import Vuex from 'vuex'
+
 Vue.use(VueResource)
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 export var router = new VueRouter()
-
-// Set up routing and match routes to components
-router.map({
-  '/home': {
-    component: Home
-  },
-  'secretquote': {
-    component: SecretQuote
-  },
-  '/login': {
-    component: Login
-  },
-  '/signup': {
-    component: Signup
-  }
-})
 
 // Redirect to the home route if any routes are unmatched
 router.redirect({
@@ -34,27 +17,3 @@ router.redirect({
 
 // Start the app on the #app div
 router.start(App, '#app')
-
-// // app entry file
-//
-// // The Vue build version to load with the `import` command
-// // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-// import Vue from 'vue'
-// import App from './App'
-// import router from './router'
-// import VueResource from 'vue-resource'
-//
-// Vue.use(VueResource)
-// Vue.config.productionTip = false
-//
-// /* Auth plugin */
-// import Auth from './auth'
-// Vue.use(Auth)
-//
-// /* eslint-disable no-new */
-// new Vue({
-//   el: '#app',
-//   router,
-//   template: '<App/>',
-//   components: { App }
-// })
