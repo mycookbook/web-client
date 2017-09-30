bdf548b28191837b3ab73decac1f9bff3a61ffbf
andela-fokosun <okosunuzflorence@gmail.com>
2017-10-01 01:19:52 +0300

    update routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,15 @@ import Router from 'vue-router'
 import VueResource from 'vue-resource'
 import Hello from '@/components/Hello'
 import SignIn from '../components/SignIn.vue'
+import SignOut from '../components/SignOut.vue'
 import Feeds from '../components/Feeds.vue'
 
 Vue.use(VueResource)
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: '/',
   routes: [{
     path: '/',
     name: 'Hello',
@@ -18,7 +21,11 @@ export default new Router({
     name: 'SignIn',
     component: SignIn
   }, {
-    path: '/:id/feeds',
+    path: '/signout',
+    name: 'SignOut',
+    component: SignOut
+  }, {
+    path: '/feeds',
     name: 'user.feeds',
     component: Feeds
   }, {
