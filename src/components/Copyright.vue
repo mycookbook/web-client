2a29f1b95c4e0275599e55bc514537860cce5769
andela-fokosun <okosunuzflorence@gmail.com>
2017-10-02 14:52:59 +0300

    implement copyright component

---
+++ src/components/Copyright.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="footer blue">
+    <span>
+      <a class="ui red label">
+        Report a bug <i class="bug icon"></i>
+      </a>
+    </span>
+    <span class="bottom aligned">
+      <span v-for="link in links">
+        <a v-bind:href="content"> {{ link }} | </a>
+      </span>
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      content: '',
+      links: ['About', 'User Stories', 'Find Cookbooks', 'Terms & Conditions', 'Data Retention Policy', 'API docs']
+    }
+  }
+}
+</script>
+<style>
+.footer {
+  position: absolute;
+  /* bottom: 0;  remove this */
+  /*margin-top: -50px; /* add this */*/
+  left: 0;
+  width: 100%;
+  padding: 15px 0;
+}
+</style>
