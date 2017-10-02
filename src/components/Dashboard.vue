81f939af493f0adf139560f19f30812563c11993
andela-fokosun <okosunuzflorence@gmail.com>
2017-10-02 17:09:00 +0300

    fix indentation

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,132 +1,45 @@
 <template>
-    <div class="ui grid" style="margin:auto">
-      <Navigation></Navigation>
-      <div class="four wide column centered grid">
-        <div class="ui segment">
-          <p>
-            popular
-          </p>
-        </div>
-      </div>
-      <div class="four wide column centered grid">
-        <div class="ui segment">
-          <p>
-            <div class="ui feed">
-  <div class="event">
-    <div class="label">
-      <img src="../assets/chef.png">
-    </div>
-    <div class="content">
-      <div class="summary">
-        <a class="user">
-          Elliot Fu
-        </a> added you as a friend
-        <div class="date">
-          1 Hour Ago
-        </div>
-      </div>
-      <div class="meta">
-        <a class="like">
-          <i class="like icon"></i> 4 Likes
-        </a>
+  <div class="ui grid" style="margin:auto">
+    <Navigation></Navigation>
+    <div class="four wide column centered grid">
+      <div class="ui segment">
+        <p>popular</p>
       </div>
     </div>
-  </div>
-  <div class="event">
-    <div class="label">
-      <img src="../assets/chef.png">
-    </div>
-    <div class="content">
-      <div class="summary">
-        <a>Helen Troy</a> added <a>2 new illustrations</a>
-        <div class="date">
-          4 days ago
-        </div>
-      </div>
-      <div class="extra images">
-        <a><img src="../assets/chef.png"></a>
-        <a><img src="../assets/chef.png"></a>
-      </div>
-      <div class="meta">
-        <a class="like">
-          <i class="like icon"></i> 1 Like
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="event">
-    <div class="label">
-      <img src="../assets/chef.png">
-    </div>
-    <div class="content">
-      <div class="summary">
-        <a class="user">
-          Jenny Hess
-        </a> added you as a friend
-        <div class="date">
-          2 Days Ago
-        </div>
-      </div>
-      <div class="meta">
-        <a class="like">
-          <i class="like icon"></i> 8 Likes
-        </a>
+    <div class="four wide column centered grid">
+      <div class="ui segment">
+        <p>
+          <div class="ui feed">
+            <div class="event">
+              <div class="label">
+                <img src="../assets/chef.png">
+              </div>
+              <div class="content">
+                <div class="summary">
+                  <a class="user">
+                    Elliot Fu
+                  </a> added you as a friend
+                  <div class="date">
+                    1 Hour Ago
+                  </div>
+                </div>
+                <div class="meta">
+                  <a class="like">
+                    <i class="like icon"></i> 4 Likes
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </p>
       </div>
     </div>
-  </div>
-  <div class="event">
-    <div class="label">
-      <img src="../assets/chef.png">
+    <div class="four wide column centered grid">
+      right
     </div>
-    <div class="content">
-      <div class="summary">
-        <a>Joe Henderson</a> posted on his page
-        <div class="date">
-          3 days ago
-        </div>
-      </div>
-      <div class="extra text">
-        Ours is a life of constant reruns. We're always circling back to where we'd we started, then starting all over again. Even if we don't run extra laps that day, we surely will come back for more of the same another day soon.
-      </div>
-      <div class="meta">
-        <a class="like">
-          <i class="like icon"></i> 5 Likes
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="event">
-    <div class="label">
-      <img src="../assets/chef.png">
-    </div>
-    <div class="content">
-      <div class="summary">
-        <a>Justen Kitsune</a> added <a>2 new photos</a> of you
-        <div class="date">
-          4 days ago
-        </div>
-      </div>
-      <div class="extra images">
-        <a><img src="../assets/chef.png"></a>
-        <a><img src="../assets/chef.png"></a>
-      </div>
-      <div class="meta">
-        <a class="like">
-          <i class="like icon"></i> 41 Likes
-        </a>
-      </div>
-    </div>
-  </div>
-</div>
-          </p>
-        </div>
-      </div>
-      <div class="four wide column centered grid">
-        right
-      </div>
       <!-- <Copyright></Copyright> -->
     </div>
-  </template>
+</template>
 
   <script>
     import Navigation from './Navigation.vue'
