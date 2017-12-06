3ba2375fa547ae0745e4c015e0eb9280be10d3f0
andela-fokosun <okosunuzflorence@gmail.com>
2017-12-06 19:51:16 +0300

    fixing hashbang wahala

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -9,6 +9,51 @@
     <br><br><br>
     <br><br><br>
 
+    <!-- <CookbookList /> -->
+    <br><br><br>
+    <div class="ui horizontal divider">
+      top recipes
+    </div>
+    <!-- <RecipeList /> -->
+    <br><br><br>
+
+    <!-- <Bottom /> -->
+  </div>
+</template>
+
+<script>
+import Navigation from './Navigation.vue'
+// import Search from './Search.vue'
+// import CookbookList from './CookbookList.vue'
+// import RecipeList from './RecipeList.vue'
+// import Bottom from './Bottom.vue'
+
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    // Search: Search,
+    // Bottom: Bottom,
+    // RecipeList: RecipeList,
+    Navigation: Navigation
+    // CookbookList: CookbookList
+  }
+}
+</script>
+
+
+<!-- <template>
+  <div class="site-content">
+    <Navigation />
+    <br><br><br>
+    <br><br><br>
+    <br><br><br><br>
+
+    <Search />
+    <br><br><br>
+    <br><br><br>
+
     <CookbookList />
     <br><br><br>
     <div class="ui horizontal divider">
@@ -40,4 +85,4 @@ export default {
     CookbookList: CookbookList
   }
 }
-</script>
+</script> -->
--- src/router/index.js
+++ src/router/index.js
@@ -1,70 +1,44 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import VueResource from 'vue-resource'
-import LogIn from '@/components/auth/Login.vue'
-import SignUp from '@/components/auth/Signup.vue'
-import SignOut from '@/components/SignOut.vue'
-import Cookbook from '@/components/Cookbook.vue'
 import LandingPage from '@/components/LandingPage'
-import Recipe from '@/components/Recipe.vue'
-import Discover from '@/components/Discover'
-import Help from '@/components/Help'
+import Register from '@/components/auth/Register'
+import Login from '@/components/auth/Login'
 
-Vue.use(VueResource)
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  hashbang: true,
-  history: true,
-  base: '/',
-  routes: [{
-    path: '/',
-    name: 'Home',
-    component: LandingPage
-  }, {
-    path: '/login',
-    name: 'LogIn',
-    component: LogIn
-  }, {
-    path: '/signup',
-    name: 'SignUp',
-    component: SignUp
-  }, {
-    path: '/signout',
-    name: 'SignOut',
-    component: SignOut
-  }, {
-    path: '/cookbook',
-    name: 'create.cookbook',
-    component: Cookbook
-  }, {
-    path: '/recipe',
-    name: 'create.recipe',
-    component: Recipe
-  }, {
-    path: '/discover',
-    name: 'Discover',
-    component: Discover
-  }, {
-    path: '/help',
-    name: 'Help',
-    component: Help
-  }, {
-    path: '/recipes/all',
-    component: {
-      template: '<div>' +
-      '<h1>Recipes</h1>' +
-      'Display all recipes' +
-      '</div>'
-    }
-  }, {
-    path: '*',
-    component: {
-      template: '<div>' +
-      '<h1>404</h1>' +
-      'Page Not Found' +
-      '</div>'
-    }
-  }]
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: LandingPage
+    }, {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    }, {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }, {
+      path: '/discover',
+      name: 'Discover',
+      component: {
+        template: '<div> Discover </div>'
+      }
+    }, {
+      path: '/help',
+      name: 'Help',
+      component: {
+        template: '<div> Help </div>'
+      }
+    }, {
+      path: '*',
+      component: {
+        template: '<div>' +
+        '<h1>404</h1>' +
+        'Page Not Found' +
+        '</div>'
+      }
+    }]
 })
