25e4163a195ed8a36b9163eeca332babf76b6288
andela-fokosun <okosunuzflorence@gmail.com>
2017-12-06 20:02:24 +0300

    building my thing jejeje

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -9,35 +9,35 @@
     <br><br><br>
     <br><br><br>
 
-    <!-- <CookbookList /> -->
+    <CookbookList />
     <br><br><br>
     <div class="ui horizontal divider">
       top recipes
     </div>
-    <!-- <RecipeList /> -->
+    <RecipeList />
     <br><br><br>
 
-    <!-- <Bottom /> -->
+    <Bottom />
   </div>
 </template>
 
 <script>
 import Navigation from './Navigation.vue'
-// import Search from './Search.vue'
-// import CookbookList from './CookbookList.vue'
-// import RecipeList from './RecipeList.vue'
-// import Bottom from './Bottom.vue'
+import Search from './Search.vue'
+import CookbookList from './CookbookList.vue'
+import RecipeList from './RecipeList.vue'
+import Bottom from './Bottom.vue'
 
 export default {
   data () {
     return {}
   },
   components: {
-    // Search: Search,
-    // Bottom: Bottom,
-    // RecipeList: RecipeList,
-    Navigation: Navigation
-    // CookbookList: CookbookList
+    Search: Search,
+    Bottom: Bottom,
+    RecipeList: RecipeList,
+    Navigation: Navigation,
+    CookbookList: CookbookList
   }
 }
 </script>
