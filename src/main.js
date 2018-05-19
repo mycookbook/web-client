852c51bdbe6b1cf37828fb93e29c9a6b9142a024
andela-fokosun <okosunuzflorence@gmail.com>
2018-05-19 18:21:15 +0100

    eslint

--- src/main.js
+++ src/main.js
@@ -11,20 +11,21 @@
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import 'semantic-ui-css/semantic.css'
-import 'semantic-ui-css'
 
-Vue.config.productionTip = false
+import 'semantic-ui-css/semantic.css';
+import 'semantic-ui-css';
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
-})
+  components: { App },
+});
 
 // window.$ = window.jQuery = require('jquery')
