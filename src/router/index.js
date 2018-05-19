dfcc9246ef023d04c44708939b8bfe02ae0aa7ce
andela-fokosun <okosunuzflorence@gmail.com>
2018-05-19 18:20:13 +0100

    chore: eslint

--- src/router/index.js
+++ src/router/index.js
@@ -1,61 +1,61 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import LandingPage from '@/components/LandingPage'
-import Register from '@/components/auth/Register'
-import Login from '@/components/auth/Login'
-import Signout from '@/components/auth/Signout'
+import Vue from 'vue';
+import Router from 'vue-router';
+import LandingPage from '@/components/LandingPage';
+import Register from '@/components/auth/Register';
+import Login from '@/components/auth/Login';
+import Signout from '@/components/auth/Signout';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Home',
-      component: LandingPage
+      component: LandingPage,
     }, {
       path: '/register',
       name: 'Register',
-      component: Register
+      component: Register,
     }, {
       path: '/login',
       name: 'Login',
-      component: Login
+      component: Login,
     }, {
       path: '/signout',
       name: 'Signout',
-      component: Signout
+      component: Signout,
     }, {
       path: '/discover',
       name: 'Discover',
       component: {
-        template: '<div> Discover </div>'
-      }
+        template: '<div> Discover </div>',
+      },
     }, {
       path: '/dashboard',
       name: 'Dashboard',
       component: {
-        template: '<div> Dashboard, welcome </div>'
-      }
+        template: '<div> Dashboard, welcome </div>',
+      },
     }, {
       path: '/help',
       name: 'Help',
       component: {
-        template: '<div> Help </div>'
-      }
+        template: '<div> Help </div>',
+      },
     }, {
       path: '/recipes/all',
       name: 'view.recipes',
       component: {
-        template: '<div>see all recipes</div>'
-      }
+        template: '<div>see all recipes</div>',
+      },
     }, {
       path: '*',
       component: {
         template: '<div>' +
         '<h1>404</h1>' +
         'Page Not Found' +
-        '</div>'
-      }
-    }]
-})
+        '</div>',
+      },
+    }],
+});
