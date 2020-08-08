1fa64eb585410981537cfa526b5efd136dd7e315
fokosun <okosunuzflorence@gmail.com>
2020-08-08 13:25:46 -0400

    WIP cookbook page redesign

--- src/components/Cookbook.vue
+++ src/components/Cookbook.vue
@@ -5,97 +5,90 @@
         <h2 class="cookbook-title">
           {{ cookbook.name }}
         </h2>
-        <div class="ui three statistics info message">
-          <div class="statistic">
-            <div class="value">
-              {{ typeof cookbook.recipes === 'undefined' ? 0 : cookbook.recipes.length }}
-            </div>
-            <div class="label">
-              Recipes
-            </div>
-          </div>
-          <div class="statistic">
-            <div class="value">
-              0
-            </div>
-            <div class="label">
-              Subscribers
-            </div>
-          </div>
-          <div class="statistic">
-            <div class="value">
-              4.74 <i class="tiny yellow star icon"></i>
-            </div>
-            <div class="label">
-              Rating <br>
-              <div class="ui star rating" data-rating="3"></div>
-            </div>
-          </div>
-        </div>
         <div v-if="cookbook">
           <div class="ui description">
             <div class="ui message">
-              <p>{{ cookbook.description }}</p>
+            	<p class="ui centered text">
+					<small>
+						{{ cookbook.description }}
+					</small>
+				</p>
             </div>
           </div>
           <br><br>
-          <div class="pushable content">
-            <div class="pusher">
-              <div class="">
-                <div class="ui meals container" v-if="typeof cookbook.recipes != 'undefined'">
-                  <div class="meal section">
-                    <div class="content active">
-                        <div class="ui grid">
-                          <div
-                          class="sixteen wide mobile column
+          <div>
+            <div>
+              <div>
+                <div v-if="typeof cookbook.recipes != 'undefined'">
+                  <div>
+                    <div>
+                        <div>
+                          <div class="sixteen wide mobile column
                                   sixteen wide tablet column
                                   eight wide computer column
                                   eight wide large screen column"
                                   v-for="recipe in cookbook.recipes"
                                   :id="recipe.id"
                             >
-                            <router-link :to="{
-                              name: 'Recipe',
-                              params: {
-                                  cookbookId: cookbook.id,
-                                  recipeId: recipe.id
-                                }
-                              }">
-                              <div class="serving card">
-                                <div class="image wrapper">
-                                  <div class="image"
-                                    :style="{ 'background-image': 'url(' + recipe.imgUrl + ')' }">
-                                  </div>
-                                </div>
-                                <div class="content">
-                                  <div class="ui medium header">
-                                     {{ recipe.name }}
-                                  </div>
-                                  <div class="tvn vertical fade clipped description">
-                                    {{ recipe.description }}
-                                  </div>
-                                  <div class="footer options">
-                                    <span class="left floated">
-                                      <a href="/" title="download recipe">
-                                        <i class="download icon"></i>
-                                        5k+ Downloads
-                                      </a>
-                                    </span>
-                                    <router-link :to="{
-                                      name: 'Recipe',
-                                      params: {
-                                          cookbookId: cookbook.id,
-                                          recipeId: recipe.id
-                                        }
-                                      }">
-                                      <button class="ui primary right floated basic button">
-                                        view recipe
-                                      </button>
-                                    </router-link>
-                                  </div>
-                                </div>
-                              </div>
-                            </router-link>
+							<div class="ui grid">
+								<div class="two column row">
+									<div class="fifteen wide column">
+										<router-link :to="{
+											name: 'Recipe',
+											params: {
+												cookbookId: cookbook.id,
+												recipeId: recipe.id
+												}
+											}">
+											<div class="ui header">
+												<h2>{{ recipe.name }}</h2>
+											</div>
+										</router-link>
+										<div>
+											<i class="clock outline icon"></i> 
+											1hr 15 mins
+										</div>
+										<div>
+											<small>
+												<em>{{ recipe.nutritional_detail}}</em>
+											</small>
+										</div>
+										<div class="ui grid">
+											<div class="three column row">
+												<div class="two wide column">
+													<router-link :to="{
+														name: 'Recipe',
+														params: {
+															cookbookId: cookbook.id,
+															recipeId: recipe.id
+															}
+														}">
+														<img class="ui medium image" :src="recipe.imgUrl">
+													</router-link>	
+												</div>
+												<div class="eleven wide column">
+													{{ recipe.description }}
+												</div>
+											</div>
+										</div>
+										<div class="ui grid">
+											<div class="ui three wide column">
+												<h6 class="ui blue header" style="text-transform: uppercase; position: absolute; top: -10%">
+													by {{ recipe.user.name }}
+												</h6>
+											</div>
+											<div class="ui thirteen wide column labels">
+												<div class="ui blue label" v-for="ingredient in recipe.ingredients">
+													{{ ingredient }}
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="one wide column">
+										<b>ad space</b>
+									</div>
+								</div>
+							</div>
                           </div>
                         </div>
                       </div>
@@ -167,7 +160,6 @@ export default {
 .cookbook-title {
   font-weight: 300;
   text-align: center;
-  padding: 65px;
   font-size: calc(30px + (26 - 14) * ((100vw - 300px) / (1600 - 300)));
 }
 .statistics {
