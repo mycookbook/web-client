bc5297e857584aa820528348d1eadf6aa61af475
fokosun <okosunuzflorence@gmail.com>
2020-08-21 23:19:28 -0400

    WIP

---
+++ src/components/Variety.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <h2>{{ title }}</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: "ChildComponent",
+  props: {
+    numbers: Array
+  },
+  data() {
+    return {
+		title: 'This is an Example component'
+    }
+  }
+};
+</script>
