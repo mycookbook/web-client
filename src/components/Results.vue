48d76368356b8a0f5afab9c3d4dfe2a125082142
fokosun <okosunuzflorence@gmail.com>
2020-08-24 04:12:20 -0400

    micro optimizations

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,54 +1,50 @@
 <template>
-<div class="pushable content">
-    <div class="pusher">
-		<div class="">
-			<div class="ui meals container" v-if="cookbooks.length > 0">
-				<div class="meal section">
-					<div class="content active">
-						<div class="ui grid">
-							<div class="sixteen wide mobile column sixteen wide tablet column eight wide computer column eight wide large screen column" v-for="cookbook in cookbooks" :id="cookbook.id">
-								<div class="serving card">
-									<div class="image wrapper">
-										<div class="image" :style="{ 'background-image': 'url(' + cookbook.bookCoverImg + ')' }"></div>
-									</div>
-									<div class="content">
-										<div class="ui sub header">
-											<span :style="{ 'float': 'right' }">
-												proudly <i :class="generateFlagClass(cookbook.flag.flag)" :title="cookbook.flag.nationality"></i>
-											</span>
-										</div>
-										<div class="ui labels">
-											<a class="ui tiny label" v-for="category in cookbook.categories" :style="{ 'background-color': getBgColor(category.color)}">
-												{{ category.name }}
-											</a>
-										</div>
-										<div class="ui medium header">
-											{{ cookbook.name }}
-										</div>
-										<div class="tvn vertical fade clipped description">
-											{{ cookbook.description }}
-										</div>
-										<div class="footer options">
-											<span>
-												<router-link :to="{name: 'Cookbook',params: {id: cookbook.id}}">
-													<button class="ui primary left floated basic button">
-														{{ cookbook.recipes.length }} Recipe(s)
-													</button>
-												</router-link>
-											</span>
-										</div>
-									</div>
+<div>
+	<div class="ui meals container" v-if="cookbooks.length > 0">
+		<div class="meal section">
+			<div class="content">
+				<div class="ui grid">
+					<div class="sixteen wide mobile column sixteen wide tablet column eight wide computer column eight wide large screen column" v-for="cookbook in cookbooks" :id="cookbook.id">
+						<div class="serving card">
+							<div class="image wrapper">
+								<div class="image" :style="{ 'background-image': 'url(' + cookbook.bookCoverImg + ')' }"></div>
+							</div>
+							<div class="content">
+								<div class="ui sub header">
+									<span :style="{ 'float': 'right' }">
+										proudly <i :class="generateFlagClass(cookbook.flag.flag)" :title="cookbook.flag.nationality"></i>
+									</span>
+								</div>
+								<div class="ui labels">
+									<a class="ui tiny label" v-for="category in cookbook.categories" :style="{ 'background-color': getBgColor(category.color)}">
+										{{ category.name }}
+									</a>
+								</div>
+								<div class="ui medium header">
+									{{ cookbook.name }}
+								</div>
+								<div class="tvn vertical fade clipped description">
+									{{ cookbook.description }}
+								</div>
+								<div class="footer options">
+									<span>
+										<router-link :to="{name: 'Cookbook',params: {id: cookbook.id}}">
+											<button class="ui primary left floated basic button">
+												{{ cookbook.recipes.length }} Recipe(s)
+											</button>
+										</router-link>
+									</span>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				<div class="tvn horizontal stroke"></div>
-			</div>
-			<div style="text-align:center!important;" v-else>
-				<p class="ui item button">Ooops!</p>
 			</div>
 		</div>
+		<div class="tvn horizontal stroke"></div>
+	</div>
+	<div style="text-align:center!important;" v-else>
+		<p class="ui item button">Ooops!</p>
 	</div>
 </div>
 </template>
@@ -75,3 +71,9 @@ export default {
 	}
 }
 </script>
+
+<style scoped>
+a.ui.label {
+	cursor: none!important;
+}
+</style>
