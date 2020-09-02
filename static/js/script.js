b2a258eb58e4051c55af199755e727cd58fee717
fokosun <okosunuzflorence@gmail.com>
2020-09-01 21:39:23 -0600

    static resources

--- static/js/script.js
+++ static/js/script.js
@@ -1,38 +1,19 @@
 /* eslint-disable */
+const query = 'sample'
 
 $(function() {
-	$('.ui.accordion').accordion({
-		exclusive: false,
+	$('#search-input').keyup(function() {
+		let query = this.value
+
+		$('.ui.search').search({
+			apiSettings: {
+				cache: 'local',
+				url: `${process.env.BASE_URL}/search?query=${query}`
+			},
+			fields: {
+				results : 'name',
+			},
+			minCharacters : 3
+		  });
 	});
-	
-	$('.ui.rating').rating({
-		maxRating: 5,
-		interactive: false,
-	});
-	
-	$('.ui.rating.interactive').rating({
-		interactive: true,
-	});
-	
-	$('.sidebar')
-	.sidebar({
-		context: $('.pushable.content'),
-		scrollLock: true,
-	})
-	.sidebar('setting', 'transition', 'push')
-	.sidebar('setting', 'mobileTransition', 'push');
-	
-	$('.serving.card, .sidebar .close.button').click(function() {
-		$('.sidebar').sidebar('toggle');
-	});
-	
-	$(".rating").rating();
-	
-	$('.special.cards .image').dimmer({
-		on: 'hover'
-	});
-	
-	$('.nut_info').popup({
-		on: 'hover'
-	})
 });
