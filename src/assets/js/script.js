9afbfc631bcc3c45b1aadc9aa8e66035ad553728
fokosun <okosunuzflorence@gmail.com>
2020-11-06 11:31:27 -0700

    init nag element

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,13 +1,19 @@
 /* eslint-disable */
 
 $(function() {
+	$('.cookie.nag').nag('show');
+
 	$('.nutr_info').popup({});
+
 	$('#addRecipeButtonTitleText').popup({
 		title: 'Know a nice little tweak to this recipe? Make it your own, click to add it!'
-	})
+	});
+
 	$('#viewRecipeSubmissionsTitleText').popup({
 		title: 'Click to view submissions'
-	})
+	});
+
 	$('.ui.rating').rating();
+	
 	$('.url.example .ui.embed').embed();
 });
