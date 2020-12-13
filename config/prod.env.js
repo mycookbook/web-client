7f3cb79badeceebb0f08ddb7287524de241e7b3e
fokosun <okosunuzflorence@gmail.com>
2020-12-12 19:01:36 -0700

    api config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL: '"https://cookbookhq-api.herokuapp.com/api/v1/"',
+  BASE_URL: '"https://api.cookbookshq.com/api/v1/"',
   IPINFO_TOKEN: '"13838312c2e092"',
 }
