faca9eb824a6fcafd9cf324bdcec665ee817e405
fokosun <okosunuzflorence@gmail.com>
2020-12-13 22:09:56 -0700

    WIP: fixing loads and performance on landing page

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -4,91 +4,94 @@
 		<div>
 			<Navigation />
 			<Search />
-			{{ isLoading }}
-			<QuickSort :filters="filters" :cookbooks="cookbooks" />
-			<div class="ui grid" style="margin-top: 15px;">
-				<div class="sixteen wide column">
-					<h3>
-						<a class="ui red tag label">
-							Save Money
-						</a>
-					</h3>
-					<img class="ui image" src="https://karmasnack.com/wp-content/uploads/2014/03/PIZZA.png" />
+			<div v-if="cookbooks">
+				<div v-if="filters()">
+					<QuickSort :filters="filters()" :cookbooks="cookbooks" />
 				</div>
-			</div>
-			<div class="ui grid">
-				<div class="ui sixteen wide column">
-					<h3>Based on popular demand</h3>
-					<div class="ui four column grid">
-						<div class="column" href="/">
-							<a href="/">
-								<img class="ui image" 
-									src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
-								/>
+				<div class="ui grid" style="margin-top: 15px;">
+					<div class="sixteen wide column">
+						<h3>
+							<a class="ui red tag label">
+								Save Money
 							</a>
-						</div>
-						<div class="column" href="/">
-							<a href="/">
-								<img class="ui image" 
-									src="https://www.eatwell101.com/wp-content/uploads/2019/12/peanut-butter-oatmeal-recipe-2.jpg" 
-								/>
-							</a>
-						</div>
-						<div class="column" href="/">
-							<a href="/">
-								<img class="ui image" 
-									src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
-								/>
-							</a>
-						</div>
-						<div class="column" href="/">
-							<a href="/">
-								<img class="ui image" 
-									src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
-								/>
-							</a>
-						</div>
-						<div class="column" href="/">
-							<a href="/">
-								<img class="ui image" 
-									src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
-								/>
-							</a>
-						</div>
-						<div class="column" href="/">
-							<a href="/">
-								<img class="ui image" 
-									src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
-								/>
-							</a>
-						</div>
-						<div class="column" href="/">
-							<a href="/">
-								<img class="ui image" 
-									src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
-								/>
-							</a>
-						</div>
-						<div class="column" href="/">
-							<a href="/">
-								<img class="ui image" 
-									src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
-								/>
-							</a>
+						</h3>
+						<img class="ui image" src="https://karmasnack.com/wp-content/uploads/2014/03/PIZZA.png" />
+					</div>
+				</div>
+				<div class="ui grid">
+					<div class="ui sixteen wide column">
+						<h3>Based on popular demand</h3>
+						<div class="ui four column grid">
+							<div class="column" href="/">
+								<a href="/">
+									<img class="ui image" 
+										src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
+									/>
+								</a>
+							</div>
+							<div class="column" href="/">
+								<a href="/">
+									<img class="ui image" 
+										src="https://www.eatwell101.com/wp-content/uploads/2019/12/peanut-butter-oatmeal-recipe-2.jpg" 
+									/>
+								</a>
+							</div>
+							<div class="column" href="/">
+								<a href="/">
+									<img class="ui image" 
+										src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
+									/>
+								</a>
+							</div>
+							<div class="column" href="/">
+								<a href="/">
+									<img class="ui image" 
+										src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
+									/>
+								</a>
+							</div>
+							<div class="column" href="/">
+								<a href="/">
+									<img class="ui image" 
+										src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
+									/>
+								</a>
+							</div>
+							<div class="column" href="/">
+								<a href="/">
+									<img class="ui image" 
+										src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
+									/>
+								</a>
+							</div>
+							<div class="column" href="/">
+								<a href="/">
+									<img class="ui image" 
+										src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
+									/>
+								</a>
+							</div>
+							<div class="column" href="/">
+								<a href="/">
+									<img class="ui image" 
+										src="https://whipperberry.com/wp-content/uploads/2016/07/Jif-Peanut-Butter-Fruity-Breakfast-Hacks-by-WhipperBerry-3.jpg" 
+									/>
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div class="ui grid">
-				<div class="ui sixteen wide column">
-					<h3>
-						The Most Epic Rice Recipe Ever by Master Chef • Taste Show
-					</h3>
-					<div class="ui embed" data-url="https://www.youtube.com/embed/S1LB4t34X08" data-placeholder=""></div>
+				<div class="ui grid">
+					<div class="ui sixteen wide column">
+						<h3>
+							The Most Epic Rice Recipe Ever by Master Chef • Taste Show
+						</h3>
+						<div class="ui embed" data-url="https://www.youtube.com/embed/S1LB4t34X08" data-placeholder=""></div>
+					</div>
 				</div>
+				<Contact />
+				<Bottom />
 			</div>
-			<Contact />
-			<Bottom />
 		</div>
 	</div>
 </div>
@@ -107,17 +110,18 @@ import Bottom from './Bottom.vue'
 export default {
 	name: "LandingPage",
 	async mounted () {
-		// store.dispatch('boot')
+		store.dispatch('boot')
 	},
 	computed: {
 		cookbooks() {
 			return this.$store.state.cookbookStore.cookbooks
-		},
+		}
+	},
+	methods: {
 		filters() {
-			return JSON.parse(this.$store.state.cookbookStore.definitions.categories.contents)
-		},
-		isLoading() {
-			return this.$store.state.resource_isLoading
+			if (this.$store.state.cookbookStore.definitions.categories !== undefined) {
+				return JSON.parse(this.$store.state.cookbookStore.definitions.categories.contents)
+			}
 		}
 	},
 	components: {
