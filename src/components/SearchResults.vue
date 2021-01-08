8f0cfa2891f72b8952c7e4991079ec0a279cd473
fokosun <okosunuzflorence@gmail.com>
2021-01-08 14:30:53 -0700

    designing search results page

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -5,7 +5,7 @@
     <div class="ui center aligned grid">
         <div class="sixteen wide computer column sixteen wide mobile column sixteen wide tablet column">
             <div class="ui huge header">
-                Showing {{ itt.length }} of {{ itt.length }} results for " {{ qu.q }} "
+                Showing {{ results.length }} of {{ results.length }} results for " {{ qu.q }} "
             </div>
         </div>
     </div>
@@ -42,25 +42,11 @@
         <div class="thirteen wide computer column sixteen wide mobile column sixteen wide tablet column">
             <div>
                 <div class="ui fluid action input">
-                    <input type="text" placeholder="Search..." :value="qu.q">
+                    <input type="text" placeholder="Start typing..." :value="qu.q">
                     <div class="ui tbb button">Search</div>
                 </div>
             </div>
             <div>
-                <span>
-                    <small>
-                        <a class="link" @click="sortBy('newest')">
-                            most recent |
-                        </a>
-                    </small>
-                </span>
-                <span>
-                    <small>
-                        <a class="link" @click="sortBy('oldest')">
-                            oldest
-                        </a>
-                    </small>
-                </span>
                 <span style="float:right!important;">
                     <small>
                         <a href="https://www.algolia.com" target="_blank">
@@ -71,7 +57,72 @@
 	        </div>
             <div class="margin-top-15"></div>
             <div>
-                {{ itt }}
+                <small>About 9,290,000 results (0.53 seconds)</small>
+            </div>
+            <div class="margin-top-15"></div>
+            <div>
+                <div><b>Ad · www.createmycookbook.com</b></div>
+                <div class="ui header" style="margin-top:5px!important;">
+                    <a href="/">Design Your Own Cookbook - CreateMyCookbook.com</a>
+                </div>
+                <div class="ui grid content container">
+                    <div style="width:80%!important;margin-left:-13px!important;">
+                        Upload Recipes and Photos. Easy to Use Cookbook Designer. Get Started for Free. 
+                        Upload Your Own Recipes &#38; Photos Makes The Perfect Sentimental Gift. 
+                    </div>
+                </div>
+            </div>
+            <br /><br />
+            <div class="tvn horizontal stroke"></div>
+            <br /><br />
+            <div>
+                <div><b>Ad · www.createmycookbook.com</b></div>
+                <div class="ui header" style="margin-top:5px!important;">
+                    <a href="/">Design Your Own Cookbook - CreateMyCookbook.com</a>
+                </div>
+                <div class="ui grid content container">
+                    <div style="width:80%!important;margin-left:-13px!important;">
+                        Upload Recipes and Photos. Easy to Use Cookbook Designer. Get Started for Free. 
+                        Upload Your Own Recipes &#38; Photos Makes The Perfect Sentimental Gift. 
+                    </div>
+                </div>
+            </div>
+            <br /><br />
+            <div class="tvn horizontal stroke"></div>
+            <div>
+                <span>
+                    <small>
+                        <a class="link" @click="sortBy('newest')">
+                            sort by most recent |
+                        </a>
+                    </small>
+                </span>
+                <span>
+                    <small>
+                        <a class="link" @click="sortBy('oldest')">
+                            oldest
+                        </a>
+                    </small>
+                </span>
+	        </div>
+            <br />
+            <div v-for="result in results" class="capitalize">
+                <div>
+                    <span>
+                        <small>
+                            <b>{{ result.resource_type }}</b> > By {{ result.author }} > {{ result.created_at }}
+                        </small>
+                    </span>
+                </div>
+                <div>
+                    <a :href="getUri(result.id)">
+                        <h4>{{ result.name }}</h4>
+                    </a>
+                </div>
+                <div>
+                    {{ (isCookbook(result.resource_type)) ? (result.description + '...') : (result.summary + '...') }}
+                </div>
+                <div class="margin-top-15"></div>
             </div>
         </div>
     </div>
@@ -83,14 +134,31 @@ import Navigation from './Navigation.vue'
 
 export default {
     name: 'SearchResults',
-    props: {
-        results: Array
-    },
     data() {
         return {
             qu: this.$route.query,
-            itt: JSON.parse(localStorage.getItem('search'))
+            results: JSON.parse(localStorage.getItem('search'))
         }
+    },
+    methods: {
+        isCookbook(type) {
+            return (type === "cookbook")
+        },
+        isRecipe(type) {
+            return (type === "recipe")
+        },
+        getUri(id) {
+            return '/#/cookbook/' + id
+        }
+    },
+    filters: {
+        truncate: function (text, length, suffix) {
+            if (text.length > length) {
+                return text.substring(0, length) + suffix;
+            } else {
+                return text;
+            }
+        },
     },
     components: {
         Navigation
@@ -107,5 +175,8 @@ export default {
 }
 .checkboxes .checkbox {
     margin: 10px;
+}
+.capitalize {
+    text-transform: capitalize!important;
 }
 </style>
