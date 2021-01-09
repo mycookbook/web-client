1350fd12f1e91fe9c963023bf33a10b672b47aa3
fokosun <okosunuzflorence@gmail.com>
2021-01-08 19:22:31 -0700

    refactor

---
+++ src/components/AdvancedSearchFilters.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+    <div style="margin-bottom:8px;font-weight:bold;">
+        Advanced Search Filters
+    </div>
+    <div class="tvn horizontal stroke"></div>
+    <div class="margin-top-15"></div>
+    <div class="checkboxes">
+        <div class="ui checkbox">
+            <label>Hide Author</label>
+            <input type="checkbox" name="showAuthor" id="showAuthor">
+        </div>
+        <div class="ui checkbox">
+            <label>Hide Cookbooks</label>
+            <input type="checkbox" name="showCookbooks" id="showCookbooks">
+        </div>
+        <div class="ui checkbox">
+            <label>Hide Recipes</label>
+            <input type="checkbox" name="showRecipes" id="showRecipes">
+        </div>
+        <div class="ui checkbox">
+            <label>Hide Varieties</label>
+            <input type="checkbox" name="showVarieties" id="showVarieties">
+        </div>
+        <div class="ui checkbox">
+            <label>Show Recipes open for orders only</label>
+            <input type="checkbox" name="openForoOders" id="openForoOders">
+        </div>
+        <div class="ui checkbox">
+            <label>Show Marketplace items only</label>
+            <input type="checkbox" name="showmarketPlace" id="showmarketPlace">
+        </div>
+        <div class="ui checkbox">
+            <label>Show Archived</label>
+            <input type="checkbox" name="showArchived" id="showArchived">
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "AdvancedSearchFilters",
+    data() {
+        return {}
+    }
+};
+</script>
--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -5,47 +5,13 @@
     <div class="ui center aligned grid">
         <div class="sixteen wide computer column sixteen wide mobile column sixteen wide tablet column">
             <div class="ui huge header">
-                Showing {{ results.length }} of {{ results.length }} results for "{{ rss }}"
+                Showing {{ results.length }} of {{ results.length }} results for "{{ showing }}"
             </div>
         </div>
     </div>
     <div class="ui grid">
         <div class="three wide computer column sixteen wide mobile column sixteen wide tablet column" style="border-right:solid #eeeeee 1px;">
-            <div style="margin-bottom:8px;font-weight:bold;">
-                Advanced Search Filters
-            </div>
-            <div class="tvn horizontal stroke"></div>
-            <div class="margin-top-15"></div>
-            <div class="checkboxes">
-                <div class="ui checkbox">
-                    <label>Hide Author</label>
-                    <input type="checkbox" name="showAuthor" id="showAuthor">
-                </div>
-                <div class="ui checkbox">
-                    <label>Hide Cookbooks</label>
-                    <input type="checkbox" name="showCookbooks" id="showCookbooks">
-                </div>
-                <div class="ui checkbox">
-                    <label>Hide Recipes</label>
-                    <input type="checkbox" name="showRecipes" id="showRecipes">
-                </div>
-                <div class="ui checkbox">
-                    <label>Hide Varieties</label>
-                    <input type="checkbox" name="showVarieties" id="showVarieties">
-                </div>
-                <div class="ui checkbox">
-                    <label>Show Recipes open for orders only</label>
-                    <input type="checkbox" name="openForoOders" id="openForoOders">
-                </div>
-                <div class="ui checkbox">
-                    <label>Show Marketplace items only</label>
-                    <input type="checkbox" name="showmarketPlace" id="showmarketPlace">
-                </div>
-                <div class="ui checkbox">
-                    <label>Show Archived</label>
-                    <input type="checkbox" name="showArchived" id="showArchived">
-                </div>
-            </div>
+            <AdvancedSearchFilters />
         </div>
         <div class="thirteen wide computer column sixteen wide mobile column sixteen wide tablet column">
             <div>
@@ -70,37 +36,7 @@
                 <small>About 9,290,000 results (0.53 seconds)</small>
             </div>
             <div class="margin-top-15"></div>
-            <div>
-                <div><b>Ad · www.createmycookbook.com</b></div>
-                <div class="ui header" style="margin-top:5px!important;">
-                    <a href="/">Design Your Own Cookbook - CreateMyCookbook.com</a>
-                </div>
-                <div class="ui grid content container">
-                    <div style="width:80%!important;margin-left:-13px!important;">
-                        Upload Recipes and Photos. Easy to Use Cookbook Designer. Get Started for Free. 
-                        Upload Your Own Recipes &#38; Photos Makes The Perfect Sentimental Gift. 
-                    </div>
-                </div>
-            </div>
-            <br /><br />
-            <div class="tvn horizontal stroke"></div>
-            <br /><br />
-            <div>
-                <div><b>Ad · www.chapters.indigo.ca</b></div>
-                <div class="ui header" style="margin-top:5px!important;">
-                    <a href="/">Bestselling Cookbooks | chapters.indigo.ca</a>
-                </div>
-                <div class="ui grid content container">
-                    <div style="width:80%!important;margin-left:-13px!important;">
-                        Join plum Plus to save an additional 10% off &#38; enjoy free shipping everyday! 
-                        Sign-up today. Save up to 40% on bestseller books in-store &#38; online at Indigo + 
-                        free shipping over $35! Curbside Pickup Available. Save 10% with Plum Plus. 
-                        1% for Childrens Literacy.
-                    </div>
-                </div>
-            </div>
-            <br /><br />
-            <div class="tvn horizontal stroke"></div>
+                <GoogleAdSense />
             <div v-show="results.length > 0">
                 <span>
                     <small>
@@ -159,6 +95,8 @@
 
 <script>
 import Navigation from './Navigation.vue'
+import AdvancedSearchFilters from './AdvancedSearchFilters.vue'
+import GoogleAdSense from './GoogleAdSense.vue'
 
 export default {
     name: 'SearchResults',
@@ -173,7 +111,7 @@ export default {
         results() {
 			return this.$store.state.searchStore.results
 		},
-        rss() {
+        showing() {
             if (this.$route.query.q !== "") {
                 if (this.searchq !== "") {
                     return this.searchq
@@ -223,7 +161,9 @@ export default {
         },
     },
     components: {
-        Navigation
+        Navigation,
+        AdvancedSearchFilters,
+        GoogleAdSense
     }
 };
 </script>
