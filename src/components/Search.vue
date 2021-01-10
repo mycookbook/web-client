fc7ead7aecae6d327e7489cd29c0b491dc603593
fokosun <okosunuzflorence@gmail.com>
2021-01-09 23:35:57 -0700

    mobile responsive remove unused code clean up

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,18 +1,19 @@
 <template>
 <div>
 	<div class="search-banner">
-		<div class="mobile hidden">
-			<div class="seven wide column caption-text">
-			<span class="explore">Discover thousands of</span>
-			<span class="food-recipes">Recipes</span>
+		<div class="ui grid center aligned">
+			<div class="sixteen wide computer column sixteen wide tablet column sixteen wide mobile column">
+				<div class="discover">
+					<span>Discover thousands of</span>
+					<span class="biro-blue">Recipes</span>
+				</div>
+			</div>
 		</div>
-		<div class="action-text">
-			food &amp; drink.
-		</div>
-		</div>
-		<div class="sixteen wide mobile only column">
-			<div class="sixteen wide column caption-text-mobile">
-				<span class="explore">Discover Recipes</span>
+		<div class="ui grid center aligned">
+			<div class="sixteen wide computer column sixteen wide tablet column sixteen wide mobile column">
+				<div class="food-drink">
+					food &amp; drink.
+				</div>
 			</div>
 		</div>
 		<AutoComplete />
@@ -24,13 +25,6 @@
 import AutoComplete from './AutoComplete.vue'
 
 export default {
-	data () {
-		return {}
-	},
-	props: {
-		filters: Array,
-		cookbooks: Array
-	},
 	components: {
 		AutoComplete: AutoComplete
 	}
@@ -38,24 +32,17 @@ export default {
 </script>
 
 <style>
-.food-recipes {
+.biro-blue {
 	color: #0160CC!important;
 }
-.caption-text {
+.discover {
 	font-size: 60px;
 	font-weight: 900;
-	text-align: center;
+	line-height: 1.1em!important;
 }
-.caption-text-mobile {
-	font-size: 30px;
-	font-weight: 900;
-	text-align: center;
-	margin: 40px;
-}
-.action-text {
+.food-drink {
 	font-weight: 300;
-	text-align: center;
-	padding: 65px;
+	padding: 10px 0 65px 0;
 	font-size: calc(30px + (26 - 14) * ((100vw - 300px) / (1600 - 300)));
 }
 .search-banner {
