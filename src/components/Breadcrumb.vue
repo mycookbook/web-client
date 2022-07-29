dd172843507c7657dc5c1be5608196f5e8d5e0a0
Obiora’s Mum <obiorasmum@Obiorass-MacBook-Pro.local>
2022-07-29 11:17:49 -0400

    wip about page

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -6,16 +6,25 @@
                 <div class="ui small breadcrumb">
                     <a class="section" href="/">back home</a>
                     <i class="left chevron icon divider"></i>
-                    <div class="active section">
-                        {{ active }}
+                    <div class="active section" v-if="activeLink === 'My Dashboard'">
+                        <a href="">
+                            {{ activeLink }}
+                        </a>
+                        > public profile
+                    </div>
+                    <div class="active section" v-else>
+                        {{ activeLink }}
                     </div>
                 </div>
             </div>
         </div>
         <div
             class="six wide computer column six wide mobile column six wide tablet column six wide large screen column">
-            <div class="ui right floated medium button">
-                <div style="cursor:none!important;">
+            <div class="ui right floated medium red button">
+                <div style="cursor:none!important;" v-if="activeLink === 'My Dashboard'">
+                    Subscribe To My Cookbooks
+                </div>
+                <div style="cursor:none!important;" v-else>
                     <i class="ui white share icon"></i> 0 Shares
                 </div>
             </div>
@@ -28,9 +37,13 @@ import Navigation from './Navigation.vue';
 
 export default {
     name: "Breadcrumb",
-    mounted() { },
     props: {
         active: String
+    },
+    computed: {
+        activeLink() {
+            return this.active
+        }
     },
     data() {
         return {
--- src/components/ContributorProfile.vue
+++ src/components/ContributorProfile.vue
@@ -1,132 +1,131 @@
 <template>
     <div class="ui container">
-    <Navigation />
-    <div class="ui grid">
-        <div class="main-content">
-            <div v-if="isLoading">
-                <RecipeCardSkeleton />
-            </div>
-            <div class="ui grid" v-else>
-                <div class="thirteen wide computer column sixteen wide mobile column">
-                    <div class="ui grid">
-                        <div class="six wide computer column sixteen wide mobile column">
-                            <img class="ui huge fluid image" :src="contributor.avatar">
-                            <div style="margin-top:15px;">
-                                <b>{{ contributor.name }}</b> ({{ contributor.pronouns }})
-                            </div>
-                            <div>
-                                Authored {{ contributor.contributions.cookbooks }}
-                                <a :href="'/cookbooks/by/@' + contributor.name_slug">cookbooks</a>
-                                and {{ contributor.contributions.recipes }}
-                                <a :href="'/recipes/by/@' + contributor.name_slug">recipes</a>
-                            </div>
-                            <div>Member since: {{ contributor.created_at }}</div>
-                            <div>
-                                <i class="ui linkify icon"></i>
-                                <a :href="'/contributors/@' + contributor.name_slug">@{{ contributor.name_slug }}</a>
-                                <div class="ui tiny link label">COPY LINK</div>
-                            </div>
-                        </div>
-                        <div class="ten wide computer column sixteen wide mobile column">
-                            <div class="ui grid">
-                                <div :title="followTitleText" class="ui red button" style="float:right;">
-                                    Subscribe
-                                </div>
-                            </div>
-                            <div style="margin-top:8%;margin-bottom:8%;"></div>
-                            <div class="tvn horizontal stroke"></div>
-                            <div style="margin-top:3%;"></div>
-                            <div>{{ contributor.about }}</div>
-                            <div style="margin-top:8%;margin-bottom:8%;"></div>
-                            <div class="ui bottom grid" v-if="contributor.contact_detail">
-                                <div class="sixteen wide column" v-if="!contributor.contact_detail.is_public">
-                                    <i class="lock icon"></i> Contact information (private mode)
-                                </div>
-                                <div class="sixteen wide column contact-labels" v-else>
-                                    <i class="unlock icon"></i> |
-                                    <span v-if="contributor.contact_detail.facebook">
-                                        facebook:
-                                        <a :href="contributor.contact_detail.facebook">
-                                            <i class="facebook black f icon"></i>
-                                        </a>
-                                    </span>
-                                    <span v-if="contributor.contact_detail.twitter">
-                                        twitter:
-                                        <a :href="contributor.contact_detail.twitter">
-                                            <i class="twitter black t icon"></i>
-                                        </a>
-                                    </span>
-                                    <span v-if="contributor.contact_detail.instagram">
-                                        instagram:
-                                        <a :href="contributor.contact_detail.instagram">
-                                            <i class="black instagram icon"></i>
-                                        </a>
-                                    </span>
-                                    <span v-if="contributor.contact_detail.office_address">
-                                        <i class="street view icon"></i>
-                                        <a :href="getMapAddr(contributor.contact_detail.office_address)"
-                                            target="_blank">
-                                            {{ contributor.contact_detail.office_address }}
-                                        </a>
-                                    </span>
-                                    <span v-if="contributor.contact_detail.phone">
-                                        phone:
-                                        <a>
-                                            <i class="black phone icon"></i>
-                                            {{ contributor.contact_detail.phone }}
-                                        </a>
-                                    </span>
-                                    <span v-if="contributor.contact_detail.calendly">
-                                        calendly:
-                                        <a :href="contributor.contact_detail.calendly" target="_blank">
-                                            <i class="calendar icon"></i>
-                                            {{ contributor.contact_detail.calendly }}
-                                        </a>
-                                    </span>
-                                    <span v-if="contributor.contact_detail.skype">
-                                        skype:
-                                        <a :href="contributor.contact_detail.skype" target="_blank">
-                                            <i class="skype black s icon"></i>
-                                            {{ contributor.contact_detail.skype }}
-                                        </a>
-                                    </span>
-                                    <span v-if="contributor.contact_detail.website">
-                                        website:
-                                        <a :href="contributor.contact_detail.website" target="_blank">
-                                            {{ contributor.contact_detail.website }}
-                                        </a>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="ui grid">
-                        <div class="sixteen wide column">
-                            <div class="ui form">
-                                <div class="field">
-                                    <label>*Send a message/Give Feedback/Ask a question:</label>
-                                    <textarea :placeholder="personalizedMsg()" disabled></textarea>
-                                </div>
-                                <div class="field">
-                                    <label>*Your email:</label>
-                                    <input type="email" placeholder="Add comma separated email addresses" disabled />
-                                </div>
-                            </div>
-                            <br />
-                            <div class="ui small disabled button tbb">
-                                Submit
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="three wide computer column sixteen wide mobile column">
-                    <div class="ui wide skyscraper test ad" data-text="Ad space"></div>
-                </div>
-                <Contact />
-                <Bottom />
-            </div>
+        <Navigation />
+        <div v-if="isLoading">
+            <RecipeCardSkeleton />
         </div>
-    </div>
+        <div v-else>
+            <br /><br /><br />
+            <Breadcrumb :active="dashboard" />
+            <div class="thirteen wide computer column sixteen wide mobile column">
+                <div class="ui grid">
+                    <div class="six wide computer column sixteen wide mobile column">
+                        <img class="ui huge fluid image" :src="contributor.avatar">
+                        <div style="margin-top:15px;">
+                            <b>{{ contributor.name }}</b> ({{ contributor.pronouns }})
+                        </div>
+                        <div>
+                            Authored {{ contributor.contributions.cookbooks }}
+                            <a :href="'/cookbooks/by/@' + contributor.name_slug">cookbooks</a>
+                            and {{ contributor.contributions.recipes }}
+                            <a :href="'/recipes/by/@' + contributor.name_slug">recipes</a>
+                        </div>
+                        <div>Member since: {{ contributor.created_at }}</div>
+                        <div>
+                            <i class="ui linkify icon"></i>
+                            <a :href="'/contributors/@' + contributor.name_slug">@{{ contributor.name_slug }}</a>
+                            <div class="ui tiny link label">COPY LINK</div>
+                        </div>
+                    </div>
+                    <div class="ten wide computer column sixteen wide mobile column">
+                        
+                        <div style="margin-top:8%;margin-bottom:8%;"></div>
+                        <div>
+                            <h2>About Me</h2>
+                        </div>
+                        <div style="margin-top:3%;"></div>
+                        <div class="tvn horizontal stroke"></div>
+
+                        <div style="margin-top:3%;"></div>
+                        
+                        <div>{{ contributor.about }}</div>
+                        <div style="margin-top:8%;margin-bottom:8%;"></div>
+                        <div class="ui bottom grid" v-if="contributor.contact_detail">
+                            <div class="sixteen wide column" v-if="!contributor.contact_detail.is_public">
+                                <i class="lock icon"></i> Contact information (private mode)
+                            </div>
+                            <div class="sixteen wide column contact-labels" v-else>
+                                <i class="unlock icon"></i> |
+                                <span v-if="contributor.contact_detail.facebook">
+                                    facebook:
+                                    <a :href="contributor.contact_detail.facebook">
+                                        <i class="facebook black f icon"></i>
+                                    </a>
+                                </span>
+                                <span v-if="contributor.contact_detail.twitter">
+                                    twitter:
+                                    <a :href="contributor.contact_detail.twitter">
+                                        <i class="twitter black t icon"></i>
+                                    </a>
+                                </span>
+                                <span v-if="contributor.contact_detail.instagram">
+                                    instagram:
+                                    <a :href="contributor.contact_detail.instagram">
+                                        <i class="black instagram icon"></i>
+                                    </a>
+                                </span>
+                                <span v-if="contributor.contact_detail.office_address">
+                                    <i class="street view icon"></i>
+                                    <a :href="getMapAddr(contributor.contact_detail.office_address)" target="_blank">
+                                        {{ contributor.contact_detail.office_address }}
+                                    </a>
+                                </span>
+                                <span v-if="contributor.contact_detail.phone">
+                                    phone:
+                                    <a>
+                                        <i class="black phone icon"></i>
+                                        {{ contributor.contact_detail.phone }}
+                                    </a>
+                                </span>
+                                <span v-if="contributor.contact_detail.calendly">
+                                    calendly:
+                                    <a :href="contributor.contact_detail.calendly" target="_blank">
+                                        <i class="calendar icon"></i>
+                                        {{ contributor.contact_detail.calendly }}
+                                    </a>
+                                </span>
+                                <span v-if="contributor.contact_detail.skype">
+                                    skype:
+                                    <a :href="contributor.contact_detail.skype" target="_blank">
+                                        <i class="skype black s icon"></i>
+                                        {{ contributor.contact_detail.skype }}
+                                    </a>
+                                </span>
+                                <span v-if="contributor.contact_detail.website">
+                                    website:
+                                    <a :href="contributor.contact_detail.website" target="_blank">
+                                        {{ contributor.contact_detail.website }}
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="ui grid">
+                    <div class="sixteen wide column">
+                        <div class="ui form">
+                            <div class="field">
+                                <label>*Send a message/Give Feedback/Ask a question:</label>
+                                <textarea :placeholder="personalizedMsg()" disabled></textarea>
+                            </div>
+                            <div class="field">
+                                <label>*Your email:</label>
+                                <input type="email" placeholder="Add comma separated email addresses" disabled />
+                            </div>
+                        </div>
+                        <br />
+                        <div class="ui small disabled button tbb">
+                            Submit
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="three wide computer column sixteen wide mobile column">
+                <div class="ui wide skyscraper test ad" data-text="Ad space"></div>
+            </div>
+            <Contact />
+            <Bottom />
+        </div>
     </div>
 </template>
 
@@ -135,6 +134,7 @@ import Navigation from './Navigation';
 import Contact from './Contact.vue';
 import Bottom from './Bottom.vue';
 import RecipeCardSkeleton from './Skeletons/RecipeCardSkeleton.vue';
+import Breadcrumb from './Breadcrumb.vue';
 
 export default {
     name: "ContributorProfile",
@@ -152,6 +152,11 @@ export default {
             return 'Follow ' + this.contributor.name + ' to learn about their new recipes.'
         }
     },
+    data() {
+        return {
+            dashboard: 'My Dashboard'
+        }
+    },
     methods: {
         getMapAddr: function ($q) {
             return ' http://maps.google.com/?q=' + $q
@@ -163,6 +168,7 @@ export default {
     components: {
         RecipeCardSkeleton,
         Navigation,
+        Breadcrumb,
         Contact,
         Bottom
     }
@@ -171,8 +177,9 @@ export default {
 
 <style scoped>
 .container {
-	margin-top: 23vh;
-} 
+    margin-top: 23vh;
+}
+
 .main-content {
     margin-top: 18vh;
 }
