8efe31e5a77805346ca61a18f931e4380f7caab7
Obiora’s Mum <obiorasmum@Obiorass-MacBook-Pro.local>
2022-07-30 15:54:23 -0400

    wip

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,6 @@ const VueRouter = new Router({
 			component: LandingPage
 		}, {
 			path: '/tiktok',
-			mode: 'history',
 			meta: {
 				middleware: (to, from, next) => {
 					const queryString = window.location.href;
