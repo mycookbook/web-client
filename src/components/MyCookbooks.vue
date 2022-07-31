df6adbedf2ccf6f8397bf4fc10fc965c1ae99ee0
Obiora’s Mum <obiorasmum@Obiorass-MacBook-Pro.local>
2022-07-31 16:32:52 -0400

    show or hide editor

--- src/components/MyCookbooks.vue
+++ src/components/MyCookbooks.vue
@@ -1,44 +1,55 @@
 <template>
 	<div>
-		<div>
-			Hide <i class="ui chevron down icon"></i>
+		<div class="hideshowicon">
+			<div v-if="inEditMode">
+				hide editor<i class="ui chevron down icon" @click="toggleEditor('hide')"></i>
+			</div>
+			<div v-else>
+				show editor<i class="ui chevron up icon"  @click="toggleEditor('show')"></i>
+			</div>
 		</div>
 		<br />
-		<div>
-			<UploadImage />
+
+		<div id="cookbook-editor">
+			<div>
+				<UploadImage />
+			</div>
+			<div class="ui horizontal divider"></div>
+			<div class="ui form">
+				<div class="field">
+					<label>Title (required*)</label>
+					<input type="text" placeholder="Enter title" />
+				</div>
+			</div>
+			<br />
+			<div class="ui form">
+				<div class="field">
+					<label>About this Cookbook? (required*)</label>
+					<textarea
+						placeholder="A very good description will be about 300 characters long. This tells users what this cookbook is about. it may encourage users to add to your cookbook thereby increasing your visibility. Make the best use for it, good luck!"></textarea>
+				</div>
+			</div>
+			<br />
+			<div class="ui form">
+				<div class="field">
+					<label>Select up to 3 categories (required*)</label>
+					<input type="text" placeholder="Comma separated list of categories e.g Vegan" />
+				</div>
+			</div>
+			<br />
+			<div class="ui form">
+				<div class="field">
+					<label>Country Flag (Optional)</label>
+					<input type="text" placeholder="Enter ISO code e.g ca for Canada, us for USA, ng for Nigeria" />
+				</div>
+			</div>
+			<div class="ui horizontal divider"></div>
+			<button class="fluid ui tbb button">Save</button>
+
 		</div>
+
 		<div class="ui horizontal divider"></div>
-		<div class="ui form">
-			<div class="field">
-				<label>Title (required*)</label>
-				<input type="text" placeholder="Enter title" />
-			</div>
-		</div>
-		<br />
-		<div class="ui form">
-			<div class="field">
-				<label>About this Cookbook? (required*)</label>
-				<textarea
-					placeholder="A very good description will be about 300 characters long. This tells users what this cookbook is about. it may encourage users to add to your cookbook thereby increasing your visibility. Make the best use for it, good luck!"></textarea>
-			</div>
-		</div>
-		<br />
-		<div class="ui form">
-			<div class="field">
-				<label>Select up to 3 categories (required*)</label>
-				<input type="text" placeholder="Comma separated list of categories e.g Vegan" />
-			</div>
-		</div>
-		<br />
-		<div class="ui form">
-			<div class="field">
-				<label>Country Flag (Optinoal)</label>
-				<input type="text" placeholder="Enter ISO code e.g ca for Canada, us for USA, ng for Nigeria" />
-			</div>
-		</div>
-		<div class="ui horizontal divider"></div>
-		<button class="fluid ui tbb button">Save</button>
-		<div class="ui horizontal divider"></div>
+		<hr />
 		<div>
 			<div>
 				<small>
@@ -92,18 +103,9 @@ import UploadImage from './UploadImage.vue';
 
 export default {
 	name: "MyCookbooks",
-	mounted() { },
-	computed: {
-		example() {
-			return true
-		}
-	},
-	props: {
-		numbers: Array
-	},
 	data() {
 		return {
-			title: 'This is an Example component'
+			inEditMode: true
 		}
 	},
 	filters: {
@@ -111,12 +113,33 @@ export default {
 	},
 	components: {
 		UploadImage
+	},
+	methods: {
+		toggleEditor(action) {
+			if (action === 'hide') {
+				$("#cookbook-editor").removeClass("show")
+				$("#cookbook-editor").addClass("hide")
+			}
+
+			if (action === 'show') {
+				$("#cookbook-editor").addClass("show")
+				$("#cookbook-editor").removeClass("hide")
+			}
+			this.inEditMode = !this.inEditMode
+		}
 	}
 };
 </script>
 
 <style scoped>
-.example-class {
-	color: #000000;
+.hideshowicon i {
+	cursor: pointer !important;
+}
+.show {
+	display: inline;
+}
+
+.hide {
+	display: none!important;
 }
 </style>
