2d55a68ea2b4be15fd31bf14d04adc3154a7ef72
Obiora’s Mum <obiorasmum@Obiorass-MacBook-Pro.local>
2022-08-06 13:09:53 -0400

    chore fixing loose ends

--- src/components/Cookie.vue
+++ src/components/Cookie.vue
@@ -1,23 +1,22 @@
 <template>
-<div>
-    <div class="ui inline cookie nag top fixed">
-		<span class="ui title">
-    		By using this website, you acknowledge that you have read and understood our 
-			<a href="/cookie-policy">Cookie Policy,</a>
-			<a href="/usage-policy">Usage Policy,</a>
-			<a href="/data-retention-policy">Data Retention Policy,</a> and our 
-			<a href="/terms-and-conditions">Terms &amp; Conditions</a>
-  		</span>
-		<span id="cookie-dismiss">
-			<i class="close icon"></i>
-		</span>
+	<div>
+		<div class="ui inline cookie nag top fixed">
+			<span class="ui title">
+				By using this website, you acknowledge that you have read and understood our
+				<a href="/#/privacy-policy">Privacy Policy,</a>
+				<a href="/usage-policy">Usage Policy,</a> and our
+				<a href="/terms-and-conditions">Terms &amp; Conditions</a>
+			</span>
+			<span id="cookie-dismiss">
+				<i class="close icon"></i>
+			</span>
+		</div>
 	</div>
-</div>
 </template>
 
 <script>
 export default {
-	data () {
+	data() {
 		return {}
 	}
 };
@@ -25,12 +24,13 @@ export default {
 
 <style scoped>
 .title a {
-	color:#dddddd; 
+	color: #dddddd;
 	text-decoration: underline;
 }
+
 #cookie-dismiss {
 	cursor: pointer;
-	float: right!important;
+	float: right !important;
 	color: white;
 }
 </style>
