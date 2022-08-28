7fbd094ff073de1c5b51738b199e538ed3977e95
Obiora’s Mum <obiorasmum@Obiorass-MacBook-Pro.local>
2022-08-28 12:41:32 -0400

    wip search page

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,100 +1,127 @@
 <template>
-<div class="ui container">
-	<Navigation />
-    <div class="margin-top-150"></div>
-    <div class="ui grid">
-        <div class="three wide computer column sixteen wide mobile column sixteen wide tablet column"></div>
-        <div class="thirteen wide computer column sixteen wide mobile column sixteen wide tablet column">
-            <div class="ui huge header">
-                Showing {{ results.length }} of {{ results.length }} results for "{{ showing }}"
-            </div>
-        </div>
-    </div>
-    <div class="ui grid">
-        <div class="three wide computer column sixteen wide mobile column sixteen wide tablet column" style="border-right:solid #eeeeee 1px;">
-            <AdvancedSearchFilters />
-        </div>
-        <div class="thirteen wide computer column sixteen wide mobile column sixteen wide tablet column">
-            <div>
-                <div class="ui fluid action input">
-                    <input type="text" placeholder="Try: Peppermint tea recipe | Cookbooks by {author's name} | Recipes by {author's name}" v-model="searchq" @keyup="searchForQuery" >
-                    <div class="ui tbb button" @click="searchForQuery">
-                        Search
-                    </div>
+    <div class="ui container">
+        <Navigation />
+        <div class="margin-top-150"></div>
+        <div class="ui grid">
+            <div class="three wide computer column sixteen wide mobile column sixteen wide tablet column"></div>
+            <div class="thirteen wide computer column sixteen wide mobile column sixteen wide tablet column">
+                <div class="ui huge header">
+                    Showing {{ results.length }} of {{ results.length }} results for "{{ showing }}"
                 </div>
             </div>
-            <div>
-                <span style="float:right!important;">
-                    <small>
-                        <a href="/">
-                            Cookbookshq <sup>&#174;</sup>
-                        </a>
-                    </small>
-                </span>
-	        </div>
-            <div class="margin-top-15"></div>
-                <GoogleAdSense />
-            <div v-show="results.length > 0">
-                <span>
-                    <small>
-                        <a class="link" @click="sortBy('newest')">
-                            most recent |
-                        </a>
-                    </small>
-                </span>
-                <span>
-                    <small>
-                        <a class="link" @click="sortBy('oldest')">
-                            oldest
-                        </a>
-                    </small>
-                </span>
-	        </div>
-            <br />
-            <div v-for="result in results" class="capitalize">
+        </div>
+        <div class="ui grid">
+            <div class="three wide computer column sixteen wide mobile column sixteen wide tablet column"
+                style="border-right:solid #eeeeee 1px;">
+                <AdvancedSearchFilters />
+            </div>
+            <div class="thirteen wide computer column sixteen wide mobile column sixteen wide tablet column">
                 <div>
-                    <span v-if="result.resource_type === 'cookbook' && result.is_locked ">
-                        <i class="ui lock icon"></i> <small>Private</small>
-                    </span>
-                    <span v-if="result.resource_type === 'cookbook' && !result.is_locked ">
-                        <i class="ui unlock icon"></i> <small>Community</small>
-                    </span>
-                    <span v-if="result.resource_type === 'recipe' && (result.author_can_take_orders)">
-                        <i class="shopping basket icon"></i>
-                    </span>
-                    <span>
+                    <div class="ui fluid action input">
+                        <input type="text"
+                            placeholder="Try: peppermint tea recipe | cookbooks by {author's name} | recipes by {author's name} | trending | top: {:number} recipes ..."
+                            v-model="searchq" @keyup="searchForQuery">
+                        <div class="ui tbb button" @click="searchForQuery">
+                            Search
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <span style="float:right!important;">
                         <small>
-                            <b>{{ result.resource_type }}</b> > 
-                            <a :href="getUri('contributor', result.username)">By {{ result.author_name }}</a> 
-                            > {{ result.created_at }}
+                            <a href="/">
+                                Cookbookshq <sup>&#174;</sup>
+                            </a>
                         </small>
                     </span>
                 </div>
-                <div>
-                    <a class="link">
-                        <div v-if="isCookbook(result.resource_type)" @click="redirectTo(result.resource_type, result.cookbook_slug, result.cookbook_id)">
-                            <h4>{{ result.cookbook_name }}</h4>
+                <div class="margin-top-25"></div>
+                <div class="ui grid">
+                    <div class="thirteen wide computer column sixteen wide mobile column sixteen wide tablet column">
+                        <GoogleAdSense />
+                    </div>
+                    <div class="three wide computer column sixteen wide mobile column sixteen wide tablet column">
+                        <h4>Top 3 most searched recipe keywords in your location</h4>
+                        <div style="padding-top:15%">
+                            <div class="ui orange progress" data-percent="74" id="example1">
+                                <div class="bar"></div>
+                                <div class="label">Smoked Salmon 74%</div>
+                            </div>
+                            <div class="ui orange progress" data-percent="74" id="example1">
+                                <div class="bar"></div>
+                                <div class="label">Pulled Pork 4%</div>
+                            </div>
+                            <div class="ui orange progress" data-percent="74" id="example1">
+                                <div class="bar"></div>
+                                <div class="label">Jamaican Jerk Chicken recipe 74%</div>
+                            </div>
                         </div>
-                        <div v-else @click="redirectTo(result.resource_type, result.recipe_slug, result.recipe_id)">
-                            <h4>{{ result.recipe_name }}</h4>
-                        </div>
-                    </a>
+                    </div>
                 </div>
-                <div>
-                    {{ (isCookbook(result.resource_type)) ? (result.description + '...') : (result.summary + '...') }}
+
+                <div v-show="results.length > 0">
+                    <span>
+                        <small>
+                            <a class="link" @click="sortBy('newest')">
+                                most recent |
+                            </a>
+                        </small>
+                    </span>
+                    <span>
+                        <small>
+                            <a class="link" @click="sortBy('oldest')">
+                                oldest
+                            </a>
+                        </small>
+                    </span>
                 </div>
-                <div class="margin-top-15"></div>
-            </div>
-            <!--<div v-show="results.length > 0">
+                <br />
+                <div v-for="result in results" class="capitalize">
+                    <div>
+                        <span v-if="result.resource_type === 'cookbook' && result.is_locked">
+                            <i class="ui lock icon"></i> <small>Private</small>
+                        </span>
+                        <span v-if="result.resource_type === 'cookbook' && !result.is_locked">
+                            <i class="ui unlock icon"></i> <small>Community</small>
+                        </span>
+                        <span v-if="result.resource_type === 'recipe' && (result.author_can_take_orders)">
+                            <i class="shopping basket icon"></i>
+                        </span>
+                        <span>
+                            <small>
+                                <b>{{ result.resource_type }}</b> >
+                                <a :href="getUri('contributor', result.username)">By {{ result.author_name }}</a>
+                                > {{ result.created_at }}
+                            </small>
+                        </span>
+                    </div>
+                    <div>
+                        <a class="link">
+                            <div v-if="isCookbook(result.resource_type)"
+                                @click="redirectTo(result.resource_type, result.cookbook_slug, result.cookbook_id)">
+                                <h4>{{ result.cookbook_name }}</h4>
+                            </div>
+                            <div v-else @click="redirectTo(result.resource_type, result.recipe_slug, result.recipe_id)">
+                                <h4>{{ result.recipe_name }}</h4>
+                            </div>
+                        </a>
+                    </div>
+                    <div>
+                        {{ (isCookbook(result.resource_type)) ? (result.description + '...') : (result.summary + '...')
+                        }}
+                    </div>
+                    <div class="margin-top-15"></div>
+                </div>
+                <!--<div v-show="results.length > 0">
                 <span style="float:right!important;">
                     <small>
                         <a href="">See more</a>
                     </small>
                 </span>
             </div>-->
+            </div>
         </div>
     </div>
-</div>
 </template>
 
 <script>
@@ -113,8 +140,8 @@ export default {
     },
     computed: {
         results() {
-			return this.$store.state.searchStore.results
-		},
+            return this.$store.state.searchStore.results
+        },
         showing() {
             if (this.$route.query.q !== "") {
                 if (this.searchq !== "") {
@@ -169,11 +196,11 @@ export default {
                 name = "Recipe"
             }
 
-            this.$router.replace({ name, params:{ slug, id } });
+            this.$router.replace({ name, params: { slug, id } });
         },
         sortBy(order) {
-			this.$store.dispatch('sort_results_by', order)
-		},
+            this.$store.dispatch('sort_results_by', order)
+        },
     },
     filters: {
         truncate: function (text, length, suffix) {
@@ -196,16 +223,24 @@ export default {
 .margin-top-15 {
     margin-top: 15px;
 }
+
+.margin-top-25 {
+    margin-top: 75px;
+}
+
 .margin-top-150 {
     margin-top: 158px;
 }
+
 .checkboxes .checkbox {
     margin: 10px;
 }
+
 .capitalize {
-    text-transform: capitalize!important;
+    text-transform: capitalize !important;
 }
+
 .link {
-    cursor: pointer!important;
+    cursor: pointer !important;
 }
 </style>
