402894c287582655f367891891fa740a9c356866
fokosun <okosunuzflorence@gmail.com>
2022-12-22 08:12:53 -0500

    small ui updates

---
+++ src/components/FourWideCircularImage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="four wide column">
+    <img class="ui mini circular image" :src="imgUrl" :title="imgTitle" style="height:31px!important;">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imgUrl: String,
+    imgTitle: String
+  }
+}
+</script>
