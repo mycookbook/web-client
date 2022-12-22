7feb15685737cd80d4d35f7852a8c9b8a77c9b43
fokosun <okosunuzflorence@gmail.com>
2022-12-22 07:25:49 -0500

    small ui fixes

--- src/components/WhoToFollowList.vue
+++ src/components/WhoToFollowList.vue
@@ -1,52 +1,52 @@
 <template>
-    <div style="background-color:#F4F4F4;border-radius: 8px; width: 100%; height: auto; padding:15px">
-        <div>
-            <h3>Who to follow</h3>
-        </div>
-        <br />
-        <div>
-            <Follow :toFollowList="toFollowList" />
-        </div>
+  <div style="background-color:#F4F4F4;border-radius: 8px; width: 100%; height: auto; padding:15px">
+    <div>
+      <h3>Who to follow</h3>
     </div>
+    <br />
+    <div>
+      <Follow :toFollowList="toFollowList" />
+    </div>
+  </div>
 </template>
 
 <script>
 import Follow from './Follow.vue';
 
 export default {
-    name: "Example",
-    data() {
-        return {
-            toFollowList: [
-                {
-                    followers: '67K',
-                    author: "Mrs Elvis",
-                    avatar: "https://hips.hearstapps.com/countryliving.cdnds.net/17/47/1511194376-cavachon-puppy-christmas.jpg",
-                    handle: "mummy-obiora",
-                },
-                {
-                    followers: '3',
-                    author: "Edmond Olson",
-                    avatar: "https://bit.ly/3cgr09f",
-                    handle: "mjay",
-                },
-                {
-                    followers: '10K',
-                    author: "Elvis Diomonde",
-                    avatar: "https://hips.hearstapps.com/countryliving.cdnds.net/17/47/1511194376-cavachon-puppy-christmas.jpg",
-                    handle: "edmond-olson",
-                }
-            ]
+  name: "Example",
+  data() {
+    return {
+      toFollowList: [
+        {
+          followers: '67K',
+          author: "Ivah Becker",
+          avatar: "https://media.licdn.com/dms/image/D5603AQFBRMLrvq2DQQ/profile-displayphoto-shrink_100_100/0/1671469161332?e=1677110400&v=beta&t=uGB99noG2oNk07sqKb3s_tYLeWikGLhR1D4kl8u0gjE",
+          handle: "mummy-obiora",
+        },
+        {
+          followers: '3',
+          author: "Marcella Frami",
+          avatar: "https://pbs.twimg.com/profile_images/1604277603501760513/0AYDgHAZ_400x400.jpg",
+          handle: "mjay",
+        },
+        {
+          followers: '10K',
+          author: "Eulah Durgan",
+          avatar: "https://miro.medium.com/fit/c/176/176/1*4AfUx9n6IbS9YdruelgsDA@2x.jpeg",
+          handle: "edmond-olson",
         }
-    },
-    components: {
-        Follow
+      ]
     }
+  },
+  components: {
+    Follow
+  }
 };
 </script>
 
 <style scoped>
 .example-class {
-    color: #000000;
+  color: #000000;
 }
 </style>
