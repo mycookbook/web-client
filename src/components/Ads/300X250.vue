c732bd5ae08881008c90b44c870477f015218151
fokosun <okosunuzflorence@gmail.com>
2022-12-24 11:38:02 -0500

    organizing campaigns

---
+++ src/components/Ads/300X250.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="sixteen wide computer column sixteen wide mobile column">
+      <a href="https://eatbinchi.com/product/binchi-box?tracking=&campaign_id=&_id=" target="_blank">
+        <img src="/static/img/campaigns/eat-binchi/vegeterian-box.png" class="ui large fluid image" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ThreehundredByTwofifty"
+};
+</script>
+
