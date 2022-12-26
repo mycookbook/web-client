0fb6394fe4f1ae372e3d8fdec218cb02c8bfc6bd
fokosun <okosunuzflorence@gmail.com>
2022-12-25 19:42:32 -0500

    wip

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,9 +1,9 @@
 module.exports = {
   NODE_ENV: '"development"',
-  APP_URL: process.env.APP_URL,
-  BASE_URL: process.env.BASE_URL,
-  IPINFO_TOKEN: process.env.IPINFO_TOKEN,
-  TALKIFY_KEY: process.env.TALKIFY_KEY,
-  TOKEN_VALIDATE_ENDPOINT: process.env.TOKEN_VALIDATE_ENDPOINT,
-  DEV_TOKEN: process.env.DEV_TOKEN
+  BASE_URL: process.env.VUE_APP_API,
+  APP_URL: process.env.VUE_APP_APP_URL,
+  IPINFO_TOKEN: process.env.VUE_APP_IPINFO_TOKEN,
+  TALKIFY_KEY: process.env.VUE_APP_TALKIFY_KEY,
+  TOKEN_VALIDATE_ENDPOINT: process.env.VUE_APP_TOKEN_VALIDATE_ENDPOINT,
+  DEV_TOKEN: process.env.VUE_APP_DEV_TOKEN
 }
