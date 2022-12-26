f09e25303ddfcfbd86d9034e59f472365255c89b
fokosun <okosunuzflorence@gmail.com>
2022-12-25 19:14:37 -0500

    fix accessing env vars from heroku

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,26 +3,23 @@ import axios from 'axios';
 import store from '../store';
 
 async function auth({ next, VueRouter }) {
-  const access_token = (store.state.access_token)
-  console.log(access_token)
+  const access_token = store.state.access_token
 
   if (!access_token) {
-    return VueRouter.push({ name: 'Register' });
+    return VueRouter.push({ name: 'Register' })
   }
 
-  if (access_token) {
-    try {
-      let result = await axios.post(process.env.TOKEN_VALIDATE_ENDPOINT, {}, {
-        headers: { 'Authorization': 'Bearer ' + access_token }
-      })
+  try {
+    let result = await axios.post(process.env.TOKEN_VALIDATE_ENDPOINT, {}, {
+      headers: { 'Authorization': 'Bearer ' + access_token }
+    })
 
-      if (result.data.validated === true) {
-        return next()
-      }
-    } catch (error) {
-      localStorage.clear();
-      return VueRouter.push({ name: 'Home' });
+    if (result.data.validated === true) {
+      return next()
     }
+  } catch (error) {
+    store.dispatch('logout')
+    return VueRouter.push({ name: 'Register' })
   }
 }
 
