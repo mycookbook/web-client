2af095abad0acce100094b2ae85f39a0daf4c17d
Kaycee <nwosukelechi90@gmail.com>
2022-12-28 18:02:16 +0100

    cleanup

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,5 +1,6 @@
 module.exports = {
   NODE_ENV: '"development"',
+<<<<<<< HEAD
   BASE_URL: process.env.VUE_APP_API,
   APP_URL: process.env.VUE_APP_APP_URL,
   IPINFO_TOKEN: process.env.VUE_APP_IPINFO_TOKEN,
@@ -7,3 +8,21 @@ module.exports = {
   TOKEN_VALIDATE_ENDPOINT: process.env.VUE_APP_TOKEN_VALIDATE_ENDPOINT,
   DEV_TOKEN: process.env.VUE_APP_DEV_TOKEN
 }
+=======
+  APP_URL: '"http://localhost:5001"',
+  BASE_URL: '"https://api.cookbookshq.com/api/v1/"',
+  IPINFO_TOKEN: '"13838312c2e092"',
+  API_KEY: process.env.API_KEY,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  SERVER_ENDPOINT_OAUTH: '"https://www.tiktok.com/auth/authorize"',
+  TIKTOK_CLIENT_KEY: '"aw2m32xifypveu7g"',
+  TIKTOK_REDIRECT_URI: '"http://localhost:8080/api/v1/auth/tiktok"',
+  TWITTER_BASE_URL: '""',
+  PINTEREST_BASE_URL: '""',
+  INSTAGRAM_BASE_URL: '""',
+  VUE_APP_AWS_SECRET_ACCESS_KEY: '""',
+  VUE_APP_AWS_ACCESS_KEY: '""',
+  VUE_APP_AWS_REGION: '""',
+  VUE_APP_AWS_BUCKET:'""',
+})
+>>>>>>> 2947501 (cleanup)
