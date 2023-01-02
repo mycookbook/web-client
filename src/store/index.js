43cf62f1f9fdceb1c3632b8620fb1f7ff7ed8dc6
fokosun <okosunuzflorence@gmail.com>
2023-01-02 08:03:11 -0500

    typescript, upload service and s3 adapter

--- src/store/index.js
+++ src/store/index.js
@@ -14,151 +14,158 @@ import { contributorStore } from './modules/contributorStore.js'
 import { searchStore } from './modules/searchStore.js'
 import { userStore } from './modules/userStore.js'
 import router from '../router/index.js'
-<<<<<<< HEAD
-=======
-import { imageUploaderStore } from './modules/imageUploaderStore'
-
->>>>>>> 0ef4ad9 (Component to upload images to AWS S3)
+import { imageUploadStore } from './modules/imageUploadStore'
 
 Vue.use(Vuex);
 Vue.use(VueResource);
 
 export default new Vuex.Store({
-  strict: process.env.NODE_ENV !== 'production',
-  state: () => ({
-    named_urls: {
-      cookbook_resources: process.env.BASE_URL + 'cookbooks',
-      recipe_resources: process.env.BASE_URL + 'recipes',
-      user_resources: process.env.BASE_URL + 'users',
-      definitions: process.env.BASE_URL + 'definitions',
-      policies: process.env.BASE_URL + 'policies',
-      ipInfo: {
-        uri: 'https://ipinfo.io',
-        token: process.env.IPINFO_TOKEN
-      }
-    },
-    resource_isLoading: false,
-    policies: {
-      cookiePolicy: '',
-      usagePolicy: '',
-      dataRetentionPolicy: '',
-      termsAndConditons: ''
-    },
-    form_errors: {
-      registration_form: {},
-      contact_form: {}
-    },
-    token: process.env.DEV_TOKEN,
-    api: {
-      client: axios,
-      options: {
-        headers: {
-          'Authorization': `Bearer ${this.token}`
+	strict: process.env.NODE_ENV !== 'production',
+	state: () => ({
+		named_urls: {
+			cookbook_resources: process.env.BASE_URL + 'cookbooks',
+			recipe_resources: process.env.BASE_URL + 'recipes',
+			user_resources: process.env.BASE_URL + 'users',
+			definitions: process.env.BASE_URL + 'definitions',
+			policies: process.env.BASE_URL + 'policies',
+			ipInfo: {
+				uri: 'https://ipinfo.io',
+				token: process.env.IPINFO_TOKEN
+			}
+		},
+		resource_isLoading: false,
+		policies: {
+			cookiePolicy: '',
+			usagePolicy: '',
+			dataRetentionPolicy: '',
+			termsAndConditons: ''
+		},
+		form_errors: {
+			registration_form: {},
+			contact_form: {}
+		},
+		token: process.env.VUE_APP_DEV_TOKEN,
+		api: {
+			client: axios,
+			options: {
+				headers: {
+					'Authorization': `Bearer ${this.token}`
+				}
+			}
+		},
+
+		response: {
+			statuses: {
+				unauthorized: 401,
+				unprocessible: 422,
+				ok: 200,
+				server: 500
+			}
+		},
+		access_token: process.env.VUE_APP_DEV_TOKEN,
+		active_user: {},
+		contributor: {},
+		username: null
+	}),
+	mutations: {
+		STORE_POLICIES(state, policies) {
+			state.policies.cookiePolicy = policies.cookiePolicy.content
+			state.policies.usagePolicy = policies.usagePolicy.content
+			state.policies.dataRetentionPolicy = policies.dataRetentionPolicy.content
+			state.policies.termsAndConditons = policies.termsAndConditions.content
+		},
+		SET_LOADING_STATE(state, status) {
+			this.state.resource_isLoading = status
+		},
+		SET_ACTIVE_USER(state, req) {
+
+			this.state.access_token = req.code
+
+			this.state.username = req.username
+
+            router.push('/')
         }
-      }
     },
-    response: {
-      statuses: {
-        unauthorized: 401,
-        unprocessible: 422,
-        ok: 200,
-        server: 500
-      }
-    },
-    access_token: null,
-    active_user: {},
-    contributor: {},
-    username: null
-  }),
-  mutations: {
-    STORE_POLICIES(state, policies) {
-      state.policies.cookiePolicy = policies.cookiePolicy.content
-      state.policies.usagePolicy = policies.usagePolicy.content
-      state.policies.dataRetentionPolicy = policies.dataRetentionPolicy.content
-      state.policies.termsAndConditons = policies.termsAndConditions.content
-    },
-    SET_LOADING_STATE(state, status) {
-      this.state.resource_isLoading = status
-    },
-    SET_ACTIVE_USER(state, req) {
+    actions: {
+        async boot(context) {
+            const base_urls = {
+                'development': 'https://api.cookbookshq.com/api/v1/',
+                'production': 'https://api.cookbookshq.com/api/v1/'
+            }
 
-      this.state.access_token = req.code
+			router.push('/')
+		},
+	},
+	actions: {
+		async boot(context) {
+			const base_urls = {
+				'development': 'http://localhost:8080/api/v1/',
+				'production': 'https://api.cookbookshq.com/api/v1/'
+			}
 
-      this.state.username = req.username
+			const definitions = base_urls[process.env.NODE_ENV] + 'definitions'
+			const cookbook_resources = base_urls[process.env.NODE_ENV] + 'cookbooks'
+			const policies = base_urls[process.env.NODE_ENV] + 'policies'
 
-      location.replace('/');
-    },
-    LOGOUT(state) {
-      this.state.access_token = null
-      this.state.active_user = {}
-
-      router.push('/')
-    }
-  },
-  actions: {
-    async boot(context) {
-      const definitions = process.env.BASE_URL + 'definitions'
-      const cookbook_resources = process.env.BASE_URL + 'cookbooks'
-      const policies = process.env.BASE_URL + 'policies'
-
-      await this.state.api.client.all([
-        this.state.api.client.get(definitions),
-        this.state.api.client.get(cookbook_resources, this.state.api.options),
-        this.state.api.client.get(policies, this.state.api.options)
-      ]).then(this.state.api.client.spread((definitions, cookbooks, policies) => {
-        context.commit("SET_LOADING_STATE", false)
-        context.commit('STORE_DEFINITIONS', definitions.data)
-        context.commit('STORE_COOKBOOKS', cookbooks.data.data)
-        context.commit('STORE_POLICIES', policies.data.response)
-      })).catch(function (error) {
-        if (error.response.status === this.state.response.statuses.unauthorized) {
-          context.commit("SET_LOADING_STATE", true)
-          console.log('malformed request, check headers')
-        } else {
-          console.log('must be a server error')
-        }
-        context.commit("SET_LOADING_STATE", false)
-      })
-    },
-    unload_global_error_object(context) {
-      context.commit("UNLOAD_GLOBAL_ERROR_OBJECT")
-    },
-    unload_global_success_object(context) {
-      context.commit("UNLOAD_GLOBAL_SUCCESS_OBJECT")
-    },
-    load_skeleton(context) {
-      context.commit("SET_LOADING_STATE", true)
-    },
-    set_active_user(context, req) {
-      context.commit("SET_ACTIVE_USER", req)
-    },
-    logout(context) {
-      context.commit("LOGOUT")
-    }
-  },
-  getters: {
-    get_cookie_policy: (store) => () => {
-      return store.policies.cookiePolicy
-    },
-    get_data_retention_policy: (store) => () => {
-      return store.policies.dataRetentionPolicy
-    },
-    get_terms_and_conditions: (store) => () => {
-      return store.policies.termsAndConditons
-    },
-    get_usage_policy: (store) => () => {
-      return store.policies.usagePolicy
-    }
-  },
-  modules: {
-    cookbookStore,
-    recipeStore,
-    subscriptionStore,
-    registerStore,
-    varietiesStore,
-    contributorStore,
-    searchStore,
-    userStore,
-  },
-  plugins: [createPersistedState()]
+			await this.state.api.client.all([
+				this.state.api.client.get(definitions),
+				this.state.api.client.get(cookbook_resources, this.state.api.options),
+				this.state.api.client.get(policies, this.state.api.options)
+			]).then(this.state.api.client.spread((definitions, cookbooks, policies) => {
+				context.commit("SET_LOADING_STATE", false)
+				context.commit('STORE_DEFINITIONS', definitions.data)
+				context.commit('STORE_COOKBOOKS', cookbooks.data.data)
+				context.commit('STORE_POLICIES', policies.data.response)
+			})).catch(function (error) {
+				if (error.response.status === this.state.response.statuses.unauthorized) {
+					context.commit("SET_LOADING_STATE", true)
+					console.log('malformed request, check headers')
+				} else {
+					console.log('must be a server error')
+				}
+				context.commit("SET_LOADING_STATE", false)
+			})
+		},
+		unload_global_error_object(context) {
+			context.commit("UNLOAD_GLOBAL_ERROR_OBJECT")
+		},
+		unload_global_success_object(context) {
+			context.commit("UNLOAD_GLOBAL_SUCCESS_OBJECT")
+		},
+		load_skeleton(context) {
+			context.commit("SET_LOADING_STATE", true)
+		},
+		set_active_user(context, req) {
+			context.commit("SET_ACTIVE_USER", req)
+		},
+		logout(context) {
+			context.commit("LOGOUT")
+		}
+	},
+	getters: {
+		get_cookie_policy: (store) => () => {
+			return store.policies.cookiePolicy
+		},
+		get_data_retention_policy: (store) => () => {
+			return store.policies.dataRetentionPolicy
+		},
+		get_terms_and_conditions: (store) => () => {
+			return store.policies.termsAndConditons
+		},
+		get_usage_policy: (store) => () => {
+			return store.policies.usagePolicy
+		}
+	},
+	modules: {
+		cookbookStore,
+		recipeStore,
+		subscriptionStore,
+		registerStore,
+		varietiesStore,
+		contributorStore,
+		searchStore,
+		userStore,
+		imageUploadStore,
+	},
+	plugins: [createPersistedState()]
 });
---
+++ src/store/modules/imageUploadStore.js
@@ -0,0 +1,27 @@
+import UploadService from '../../services/UploadService'
+
+export const imageUploadStore = {
+    mutations: {
+        SET_IMAGEPATH(state, response) {
+            console.log(response)
+            // this.state.imagePath = newState.data.imagePath
+
+            //todos:
+            //1. if response.error set the error in the state object, you can watch for this
+            //using computed props in the ImgaeUpload component, that way if there is an error 
+            //it can be displayed to the use in real time
+
+            //if !response.error then go ahead and use the image url as needed
+        }
+    },
+    actions: {
+        async upload_to_s3(context, file) {
+            const service = new UploadService()
+            context.commit('SET_IMAGEPATH', service.upload(file))
+        },
+        delete_from_s3(key) {
+            const service = new UploadService()
+            service.delete(key)
+        }
+    }
+}
