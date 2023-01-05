296ca7b2828348718d9862ef103853bbf17c41fe
Kaycee <nwosukelechi90@gmail.com>
2023-01-05 15:35:06 +0100

    Changing recipe description to RichText

--- src/components/MyRecipes.vue
+++ src/components/MyRecipes.vue
@@ -140,7 +140,7 @@ export default {
             return this.$store.state.contributor.recipes
         },
         editorSettings() {
-            return { theme: 'snow', font }
+            return { theme: 'snow' }
         }
     },
     data() {
