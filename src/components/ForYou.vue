2118f8cefde3aa973b227ddfaec17add654fbd0f
fokosun <okosunuzflorence@gmail.com>
2023-06-07 23:06:40 -0400

    how about adding more dummy shit

--- src/components/ForYou.vue
+++ src/components/ForYou.vue
@@ -60,17 +60,7 @@
         </div>
         <br />
         <div class="ui divider"></div>
-        <br />
-        <div style="float:right">
-            <select class="ui dropdown">
-                <option value="0">sort alphabetically</option>
-                <option value="1">filter by latest</option>
-                <option value="0">filter by oldest</option>
-                <option value="0">taking orders</option>
-                <option value="0">shuffle</option>
-            </select>
-        </div>
-        <br /><br /><br /><br />
+        <br /><br />
         <div style="background-color: rgb(244, 244, 244); padding:15px!important;">
             <div class="ui items">
                 <div class="item">
@@ -122,6 +112,29 @@
                         src="https://images.unsplash.com/photo-1641898378716-1f38ec04bb0f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1674&q=80">
                 </a>
             </div>
+        </div>
+        <br /><br />
+        <div style="background-color: rgb(244, 244, 244); padding:15px!important;">
+            <div class="ui centered header">People with similar interests also viewed the following recipes</div>
+            <div>
+                <div class="ui small centered images" style="margin-left:5%!important;">
+                    <img
+                        src="https://images.unsplash.com/photo-1609534767446-96f9d43c5bf4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTh8OENrYldOUXJGQnN8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60">
+                    <img
+                        src="https://images.unsplash.com/photo-1565733618599-cb82f14f34ac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTd8OENrYldOUXJGQnN8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60">
+                    <img
+                        src="https://images.unsplash.com/photo-1585937421612-70a008356fbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NnxOMFNneTRmN2h5QXx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60">
+                    <img
+                        src="https://images.unsplash.com/photo-1505253758473-96b7015fcd40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8N3xOMFNneTRmN2h5QXx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60">
+                    <img
+                        src="https://images.unsplash.com/photo-1539102113920-32874e87fc97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTB8OENrYldOUXJGQnN8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60">
+                    <img
+                        src="https://images.unsplash.com/photo-1448832328263-620a1a483228?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTR8MjE4MDE2Nnx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60">
+                </div>
+            </div>
+        </div>
+        <div>
+            <a>back to top</a>
         </div>
     </div>
 </template>
