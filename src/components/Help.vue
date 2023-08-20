acf184ebfe7cd87930daa1680b26601905c1735a
fokosun <okosunuzflorence@gmail.com>
2023-08-19 21:27:49 -0400

    small updates
    small updates
    
    small updates
    
    small updates
    
    small updates

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -10,7 +10,7 @@
                     </div>
                     <div class="results"></div>
                 </div>
-                <div class="ui vertical menu">
+                <div class="ui vertical fluid menu">
                     <a class="item" href="/#/help?doc=query-syntax">
                         <h4 class="ui header">Search Query Syntax</h4>
                         <small>Learn how to get fine-grained results using our homegrown query syntax</small>
@@ -29,20 +29,11 @@
                     </a>
                 </div>
             </div>
-            <div class="sixteen wide mobile only">
-                <div class="ui three item menu" style="margin-left:15px!important;">
-                    <a class="item" href="/#/help?doc=query-syntax">
-                        <h4 class="ui header">Search Query Syntax</h4>
-                    </a>
-                    <a class="item" href="/#/help?doc=templates">
-                        <h4 class="ui header">Templates</h4>
-                    </a>
-                </div>
-            </div>
             <div class="eight wide computer column sixteen wide mobile column sixteen wide tablet column">
-                <div style="padding:25px!important;">
-                    <h1>Help Docs</h1>
-
+                <div>
+                    <h1 style="padding:0 5px!important;text-transform: capitalize!important;">
+                        {{ pageTitle }}
+                    </h1>
                     <div v-if="helpDoc == 'query-syntax' || helpDoc == undefined">
                         <div class="ui info message" v-if="helpDoc == 'query-syntax' || helpDoc == undefined">
                             <p>
@@ -53,11 +44,9 @@
                         </div>
                         <QuerySyntax />
                     </div>
-
                     <div v-if="helpDoc == 'templates'">
                         <Templates />
                     </div>
-
                     <div v-if="helpDoc == 'faqs'">
                         <div class="ui category search">
                             <div class="ui icon fluid input">
@@ -71,8 +60,8 @@
                             <i>Can't find what you're looking for? <a href="mailto:cookbookshq@gmail.com">Send us an
                                     email</a></i>
                         </small>
-                        <br /><br /><br />
                         <div v-if="relevant_question.length == 0 || relevant_question.includes(1)">
+                            <br /><br />
                             <h3>Q: Why am I not able to log in with TikTok?</h3>
                             <div>
                                 <p>
@@ -93,6 +82,7 @@
                             </div>
                         </div>
                         <div v-if="relevant_question.length == 0 || relevant_question.includes(2)">
+                            <br />
                             <h3>Q: Someone stole my recipe from my website, how do I take it down?</h3>
                             <div>
                                 <p>
@@ -111,16 +101,16 @@
                                 </p>
                             </div>
                         </div>
-                        <br />
                         <div v-if="relevant_question.length == 0 || relevant_question.includes(3)">
+                            <br />
                             <h3>Q: What are the terms and conditions of this website?</h3>
                             <div>
                                 The terms and conditions can be found <a href="#/terms-and-conditions">here.</a> We
                                 encourage you to use this platform <a href="#/contributing-guidelines">responsibly.</a>
                             </div>
                         </div>
-                        <br />
                         <div v-if="relevant_question.length == 0 || relevant_question.includes(4)">
+                            <br />
                             <h3>Q: What is Cookbooks?</h3>
                             <div>
                                 We're glad you're asking! Cookbooks is a repository of recipes, food and drinks. It allows
@@ -131,8 +121,8 @@
                                 services.
                             </div>
                         </div>
-                        <br />
                         <div v-if="relevant_question.length == 0 || relevant_question.includes(5)">
+                            <br />
                             <h3>Q: How do I create a recipe?</h3>
                             <div>
                                 To create a recipe, you need to be <a href="/#/signin">logged in</a> using your
@@ -140,12 +130,21 @@
                                 recipes right away.
                             </div>
                         </div>
-                        <br />
                         <div v-if="relevant_question.length == 0 || relevant_question.includes(6)">
+                            <br />
                             <h3>Q: How can I promote my grocery shop or business on this website?</h3>
                             <div>
                                 This capability is provided as a premium feature. You will need a subscription to enable
                                 these functionalities. TBD
+                            </div>
+                        </div>
+                        <div v-if="relevant_question.length == 0 || relevant_question.includes(7)">
+                            <br />
+                            <h3>Q: How can I create an account?</h3>
+                            <div>
+                                All that you need to do in order to start creating recipes is to <a href="/#/signin">sign
+                                    in</a> with your existing
+                                TikTok account.
                             </div>
                         </div>
                     </div>
@@ -197,67 +196,20 @@ export default {
         return {
             advQuery: '',
             q: '',
-            faqs: Object.values(faqs.data),
-        };
-    },
-    methods: {
-        search(e) {
-            if (e.which == 13) {
-                if (e.target.placeholder == 'Advanced Search') {
-                    this.$router.push({ name: 'SearchResults', query: { q: this.advQuery } })
-                } else {
-                    const words = this.q.replace(/[&\/\\#, +()$~%.'":*?<>{}]/g, ' ').split(' ')
-                    const faqs = this.faqs
-                    let res = []
-
-                    words.forEach(function (w) {
-                        if (w.length > 0) {
-                            for (let i = 0; i < faqs.length; i++) {
-                                if (faqs[i].keywords.includes(w)) {
-                                    res.push(faqs[i])
-                                }
-                            }
-                        }
-                    })
-
-                    if (res.length == 0) {
-                        location.reload()
-                    } else {
-                        let matches = res.map(function (d) {
-                            return d.question
-                        })
-
-                        var dedupedQuestions = matches.filter(function (d, i) {
-                            return matches.indexOf(d) === i;
-                        });
-
-                        let dedupedResults = []
-
-                        dedupedQuestions.map(function (d) {
-                            for (let i = 0; i < faqs.length; i++) {
-                                if (faqs[i].question === d) {
-                                    dedupedResults.push(faqs[i])
-                                }
-                            }
-                        })
-
-                        this.faqs = dedupedResults
-                    }
-                }
-            }
-        }
-    },
-    data() {
-        return {
-            advQuery: '',
-            q: '',
             keywords: {
                 'login': [1],
+                'tiktok': [1],
+                'signin': [1],
                 'logging': [1],
+                'account': [1, 7],
                 'recipe': [2, 5],
+                'recipes': [2, 5],
                 'report': [2],
+                'plagiarism': [2],
                 'about': [4],
-                'search': [3]
+                'search': [3],
+                'terms': [3],
+                'conditions': [3]
             },
             relevant_question: []
         };
@@ -268,30 +220,38 @@ export default {
                 if (e.target.placeholder == 'Advanced Search') {
                     this.$router.push({ name: 'SearchResults', query: { q: this.advQuery } })
                 } else {
-                    let words = this.q.replace(/[&\/\\#, +()$~%.'":*?<>{}]/g, ' ').split(' ')
-                    const objKeys = Object.keys(this.keywords)
+                    if (this.q.length == 0 || this.q.length <= 3) {
+                        this.relevant_question = []
+                    } else {
+                        let words = this.q.replace(/[&\/\\#, +()$~%.'":*?<>{}]/g, ' ').split(' ')
+                        const objKeys = Object.keys(this.keywords)
 
-                    let keywords = objKeys.filter(function (i) {
-                        return words.includes(i)
-                    })
+                        let keywords = objKeys.filter(function (i) {
+                            return words.includes(i)
+                        })
 
-                    let k = this.keywords
+                        let k = this.keywords
 
-                    let questions = []
+                        let questions = []
 
-                    keywords.forEach(function (el) {
-                        if (k.hasOwnProperty(el)) {
-                            k[el].forEach(function (m) {
-                                questions.push(m)
-                            })
+                        keywords.forEach(function (el) {
+                            if (k.hasOwnProperty(el)) {
+                                k[el].forEach(function (m) {
+                                    questions.push(m)
+                                })
+                            }
+                        })
+
+                        if (questions.length > 0) {
+                            let deduped = questions.filter(function (s, index) {
+                                return questions.indexOf(s) === index;
+                            });
+
+                            this.relevant_question = deduped
+                        } else {
+                            alert('Nothing to show you!')
                         }
-                    })
-
-                    let deduped = questions.filter(function (s, index) {
-                        return questions.indexOf(s) === index;
-                    });
-
-                    this.relevant_question = deduped
+                    }
                 }
             }
         }
