4496800d17b30a8bfb25bdf467d2a7ce86d3d6eb
fokosun <okosunuzflorence@gmail.com>
2023-08-20 01:11:42 -0400

    better handle faqs

---
+++ data/faqs.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+        'question': 'Q: Why am I not able to log in with TikTok?',
+        'answer': '<p>There are a few reasons why you are experiencing issues logging in to cookbooks with your tiktok account.</p><ul><li>Your profile is private. Try making your profile public.</li><li>We need access to your tiktok videos. Allow access to your videos on the tiktok login screen. Click edit access and enable the <i><span class="highlight-yellow">Read your public videos on TikTok</span></i> button.</li></ul>',
+        'keywords': ['login', 'logging', 'tiktok', 'signin', 'account']
+    }, {
+        'question': 'Q: Someone stole my recipe from my website, how do I take it down?',
+        'answer': `We are so sorry to hear that! Please note that we take matters of <a href=/#/terms-and-conditions class=highlight-yellow>Intellectual property rights</a> very seriously. We do not encourage the stealing of other people's intellectual property. Kindly let us know the recipe in question by using the report recipe tool. It can be found on the top right corner on the recipe page.`,
+        'keywords': ['recipe', 'recipes', 'report', 'plagiarism']
+    }, {
+        'question': 'Q: What are the terms and conditions of this website?',
+        'answer': 'The terms and conditions can be found <a href="#/terms-and-conditions">here.</a> We encourage you to use this platform <a href="#/contributing-guidelines">responsibly.</a>',
+        'keywords': ['search', 'terms', 'conditions']
+    }, {
+        'question': 'Q: What is Cookbooks?',
+        'answer': `We're glad you're asking! Cookbooks is a repository of recipes, food and drinks. It allows users such as your self to <a href=/#/search?q=%3Atags%7Ccookbooks%20vegan>search</a> for an unlimited number of recipes absolutely free of charge and does not require you to be a user on the platform to do so. In addition, it provides premium features that can help small business owners moneytize their goods and services.`,
+        'keywords': ['about']
+    }, {
+        'question': 'Q: How do I create a recipe?',
+        'answer': 'To create a recipe, you need to be <a href="/#/signin">logged in</a> using your tiktok account. You will be redirected to your dashboard page, where you can start creating recipes right away.',
+        'keywords': ['recipe', 'recipes']
+    }, {
+        'question': 'Q: How can I promote my grocery shop or business on this website?',
+        'answer': 'This capability is provided as a premium feature. You will need a subscription to enable these functionalities. TBD',
+        'keywords': ['recipe', 'recipes']
+    }, {
+        'question': 'Q: How can I create an account?',
+        'answer': 'All that you need to do in order to start creating recipes is to <a href="/#/signin">sign in</a> with your existing TikTok account.',
+        'keywords': ['account']
+    }
+];
+
+module.exports = {
+    data
+};
