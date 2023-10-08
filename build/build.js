3f4b30f02fb78e47ae728f95e8f5fdc4c02111ce
fokosun <okosunuzflorence@gmail.com>
2023-10-08 03:03:58 -0400

    upgrade to webpack5

--- build/build.js
+++ build/build.js
@@ -1,7 +1,5 @@
 require('./check-versions')()
 
-process.env.NODE_ENV = 'production'
-
 var ora = require('ora')
 var rm = require('rimraf')
 var path = require('path')
@@ -10,7 +8,7 @@ var webpack = require('webpack')
 var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
 
-var spinner = ora('building for production...')
+var spinner = ora('building for ... ' + process.env.NODE_ENV)
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
